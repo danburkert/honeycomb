4b9d4bba72c1e0383a28ffbe05e8eb243e7913c2
Sean Howell <showell@nic-showell.local>
2012-07-25 15:25:03 -0400

    Making the mysql plugin have better code style

---
+++ mysql-5.5.25a/storage/cloud/CloudHandler.cc
@@ -0,0 +1,141 @@
+#ifdef USE_PRAGMA_IMPLEMENTATION
+#pragma implementation        // gcc: Class implementation
+#endif
+
+#include "sql_priv.h"
+#include "sql_class.h"           // MYSQL_HANDLERTON_INTERFACE_VERSION
+#include "CloudHandler.h"
+#include "probes_mysql.h"
+#include "sql_plugin.h"
+
+/*  Undefining min and max macros defined by MySQL, because they cause problems
+ *  with the STL min and max functions (thrift includes the STL)
+*/
+#ifdef min
+#undef min
+#endif
+#ifdef max
+#undef max
+#endif
+
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+#include <protocol/TBinaryProtocol.h>
+#include <transport/TSocket.h>
+#include <transport/TTransportUtils.h>
+
+#include "gen-cpp/Engine.h"
+
+using namespace apache::thrift;
+using namespace apache::thrift::protocol;
+using namespace apache::thrift::transport;
+using namespace boost;
+
+using namespace com::nearinfinity::hbase_engine;
+
+CloudHandler::ha_cloud(handlerton *hton, TABLE_SHARE *table_arg)
+  :handler(hton, table_arg)
+{}
+
+int CloudHandler::open(const char *name, int mode, uint test_if_locked)
+{
+  DBUG_ENTER("CloudHandler::open");
+
+  if (!(share = get_share(name, table)))
+    DBUG_RETURN(1);
+  thr_lock_data_init(&share->lock,&lock,NULL);
+  //shared_ptr<TTransport> socket(new TSocket("localhost", 9090));
+  //shared_ptr<TTransport> transport(new TBufferedTransport(socket));
+  //shared_ptr<TProtocol> protocol(new TBinaryProtocol(transport));
+  //EngineClient client(protocol);
+
+  //try {
+  //transport->open();
+
+  //client.open();
+
+  //transport->close();
+  //} catch (TException &tx) {
+  //printf("ERROR: %s\n", tx.what());
+  //} 
+  DBUG_RETURN(0);
+}
+
+int CloudHandler::close(void)
+{
+  DBUG_ENTER("CloudHandler::close");
+  DBUG_RETURN(free_share(share));
+}
+
+int CloudHandler::write_row(uchar *buf)
+{
+  DBUG_ENTER("CloudHandler::write_row");
+  DBUG_RETURN(0);
+}
+
+int CloudHandler::update_row(const uchar *old_data, uchar *new_data)
+{
+  DBUG_ENTER("CloudHandler::update_row");
+  DBUG_RETURN(HA_ERR_WRONG_COMMAND);
+}
+
+int CloudHandler::delete_row(const uchar *buf)
+{
+  DBUG_ENTER("CloudHandler::delete_row");
+  DBUG_RETURN(HA_ERR_WRONG_COMMAND);
+}
+
+int CloudHandler::rnd_init(bool scan)
+{
+  DBUG_ENTER("CloudHandler::rnd_init");
+  DBUG_RETURN(HA_ERR_WRONG_COMMAND);
+}
+
+int CloudHandler::external_lock(THD *thd, int lock_type)
+{
+  DBUG_ENTER("CloudHandler::external_lock");
+  DBUG_RETURN(0);
+}
+
+int CloudHandler::rnd_next(uchar *buf)
+{
+  int rc;
+  DBUG_ENTER("CloudHandler::rnd_next");
+  MYSQL_READ_ROW_START(table_share->db.str, table_share->table_name.str, TRUE);
+  rc= HA_ERR_END_OF_FILE;
+  MYSQL_READ_ROW_DONE(rc);
+  DBUG_RETURN(rc);
+}
+
+void CloudHandler::position(const uchar *record)
+{
+  DBUG_ENTER("CloudHandler::position");
+  DBUG_VOID_RETURN;
+}
+
+int CloudHandler::rnd_pos(uchar *buf, uchar *pos)
+{
+  int rc;
+  DBUG_ENTER("CloudHandler::rnd_pos");
+  MYSQL_READ_ROW_START(table_share->db.str, table_share->table_name.str,
+                       TRUE);
+  rc= HA_ERR_WRONG_COMMAND;
+  MYSQL_READ_ROW_DONE(rc);
+  DBUG_RETURN(rc);
+}
+
+int CloudHandler::create(const char *name, TABLE *table_arg,
+                       HA_CREATE_INFO *create_info)
+{
+  DBUG_ENTER("CloudHandler::create");
+  DBUG_RETURN(0);
+}
+
+THR_LOCK_DATA **CloudHandler::store_lock(THD *thd, THR_LOCK_DATA **to, enum thr_lock_type lock_type)
+{
+  if (lock_type != TL_IGNORE && lock.type == TL_UNLOCK)
+    lock.type=lock_type;
+  *to++= &lock;
+  return to;
+}
---
+++ mysql-5.5.25a/storage/cloud/CloudHandler.h
@@ -0,0 +1,84 @@
+#ifdef USE_PRAGMA_INTERFACE
+#pragma interface			/* gcc class implementation */
+#endif
+
+#include "my_global.h"                   /* ulonglong */
+#include "thr_lock.h"                    /* THR_LOCK, THR_LOCK_DATA */
+#include "handler.h"                     /* handler */
+#include "my_base.h"                     /* ha_rows */
+
+#include "CloudShare.h"
+
+class CloudHandler : public handler
+{
+private:
+  THR_LOCK_DATA lock;      ///< MySQL lock
+  CloudShare *share;    ///< Shared lock info
+
+public:
+  CloudHandler(handlerton *hton, TABLE_SHARE *table_arg);
+  ~CloudHandler()
+  {
+  }
+
+  const char *table_type() const 
+  { 
+    return "cloud";
+  }
+
+  const char *index_type(uint inx) 
+  {
+    return "HASH"; 
+  }
+
+  ulonglong table_flags() const
+  {
+    return HA_BINLOG_STMT_CAPABLE;
+  }
+
+  ulong index_flags(uint inx, uint part, bool all_parts) const
+  {
+    return 0;
+  }
+
+  uint max_supported_record_length() const 
+  {
+    return HA_MAX_REC_LENGTH; 
+  }
+
+  uint max_supported_keys() const 
+  {
+    return 0; 
+  }
+
+  uint max_supported_key_parts() const 
+  {
+    return 0; 
+  }
+
+  uint max_supported_key_length() const 
+  {
+    return 0; 
+  }
+
+  virtual double scan_time() 
+  { 
+    return (double) (stats.records+stats.deleted) / 20.0+10; 
+  }
+
+  virtual double read_time(uint, uint, ha_rows rows)
+  { 
+    return (double) rows /  20.0+1; 
+  }
+
+  int open(const char *name, int mode, uint test_if_locked);    // required
+  int close(void);                                              // required
+  int rnd_init(bool scan);                                      //required
+  int rnd_next(uchar *buf);                                     ///< required
+  int rnd_pos(uchar *buf, uchar *pos);                          ///< required
+  void position(const uchar *record);                           ///< required
+  int info(uint);                                               ///< required
+  int external_lock(THD *thd, int lock_type);                   ///< required
+  int create(const char *name, TABLE *form, HA_CREATE_INFO *create_info); ///< required
+  THR_LOCK_DATA **store_lock(THD *thd, THR_LOCK_DATA **to, enum thr_lock_type lock_type);     ///< required
+};
