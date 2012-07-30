12ed73a7d74df349ebe33477fefb2c8704e6e4fa
Dan Burkert <dburkert@nearinfinity.com>
2012-07-30 11:12:01 -0400

    strip out cmake garbage

--- mysql-5.5.25a/extra/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/showell/Development/mysql-cloud-engine/mysql-5.5.25a/extra
-# Build directory: /Users/showell/Development/mysql-cloud-engine/mysql-5.5.25a/extra
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- mysql-5.5.25a/sql/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/showell/Development/mysql-cloud-engine/mysql-5.5.25a/sql
-# Build directory: /Users/showell/Development/mysql-cloud-engine/mysql-5.5.25a/sql
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
