4f84ce308e9085b8a97f41dad5fab0a89a061115
Sean Howell <showell@nic-showell.local>
2012-07-30 10:08:14 -0400

    Removed cmake stuff

---
+++ mysql-5.5.25a/storage/cloud/CloudShare.h
@@ -0,0 +1,18 @@
+#ifndef CLOUD_SHARE_H
+#define CLOUD_SHARE_H
+
+typedef struct st_cloud_share {
+  char *table_name;
+  char data_file_name[FN_REFLEN];
+  uint table_name_length, use_count;
+  my_bool is_log_table;
+  mysql_mutex_t mutex;
+  THR_LOCK lock;
+  bool crashed;             /* Meta file is crashed */
+  ha_rows rows_recorded;    /* Number of rows in tables */
+  uint data_file_version;   /* Version of the data file used */
+} CloudShare;
+
+static HASH cloud_open_tables;
+static mysql_mutex_t cloud_mutex;
+#endif
