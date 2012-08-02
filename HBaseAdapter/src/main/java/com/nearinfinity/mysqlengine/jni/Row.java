83f17ab97000019f353acfd4683c88664fa5c2e9
John Edstrom <jedstrom@nearinfinity.com>
2012-08-02 10:48:09 -0400

    Updated Row API

--- HBaseAdapter/src/main/java/com/nearinfinity/mysqlengine/jni/Row.java
+++ HBaseAdapter/src/main/java/com/nearinfinity/mysqlengine/jni/Row.java
@@ -1,5 +1,6 @@
 package com.nearinfinity.mysqlengine.jni;
 
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -10,13 +11,31 @@ import java.util.Map;
  * To change this template use File | Settings | File Templates.
  */
 public class Row {
-    private Map<String, byte[]> values;
+    private Map<String, byte[]> rowMap;
 
-    public Map<String, byte[]> getValues() {
-        return values;
+    public Map<String, byte[]> getRowMap() {
+        return rowMap;
     }
 
-    public void setValues(Map<String, byte[]> values) {
-        this.values = values;
+    public void setRowMap(Map<String, byte[]> rowMap) {
+        this.rowMap = new HashMap<String, byte[]>(rowMap);
+    }
+
+    public String[] getKeys() {
+        String[] keyArray = new String[rowMap.size()];
+        int i = 0;
+        for (String key : rowMap.keySet()) {
+            keyArray[i++] = key;
+        }
+        return keyArray;
+    }
+
+    public byte[][] getValues() {
+        byte[][] valueArray = new byte[rowMap.size()][];
+        int i = 0;
+        for (Map.Entry<String, byte[]> entry : rowMap.entrySet()) {
+            valueArray[i++] = entry.getValue();
+        }
+        return valueArray;
     }
 }
