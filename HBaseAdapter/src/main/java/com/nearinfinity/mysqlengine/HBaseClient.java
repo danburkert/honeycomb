cf864efd9045b893135582314d8740b9a5efc084
John Edstrom <jedstrom@nearinfinity.com>
2012-08-07 11:25:28 -0400

    Updated logging

--- HBaseAdapter/src/main/java/com/nearinfinity/mysqlengine/HBaseClient.java
+++ HBaseAdapter/src/main/java/com/nearinfinity/mysqlengine/HBaseClient.java
@@ -256,6 +256,7 @@ public class HBaseClient {
                 .array();
 
         Scan scan = new Scan(startRow, endRow);
+        scan.setCaching(10);
 
         return table.getScanner(scan);
     }
