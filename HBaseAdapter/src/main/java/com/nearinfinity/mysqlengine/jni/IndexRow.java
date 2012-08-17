f4898a53de0e491074eca8dc125e5400689ed3fc
John Edstrom <jedstrom@nearinfinity.com>
2012-08-17 11:46:45 -0400

    All calls to index_read or index_next now update the this->ref pointer so that subsequent calls to position will maintain correct value

---
+++ HBaseAdapter/src/main/java/com/nearinfinity/mysqlengine/jni/IndexRow.java
@@ -0,0 +1,50 @@
+package com.nearinfinity.mysqlengine.jni;
+
+import org.apache.hadoop.hbase.util.Bytes;
+import org.apache.log4j.Logger;
+
+import java.nio.ByteBuffer;
+import java.util.UUID;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jedstrom
+ * Date: 8/17/12
+ * Time: 10:50 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class IndexRow {
+    private byte[] unireg;
+    private byte[] uuid;
+
+    private static final Logger logger = Logger.getLogger(IndexRow.class);
+
+    public IndexRow() {
+        this.unireg = null;
+        this.uuid = null;
+    }
+
+    public IndexRow(byte[] unireg, byte[] uuid) {
+        this.unireg = unireg;
+        this.uuid = uuid;
+    }
+
+    public byte[] getUnireg() {
+        return this.unireg;
+    }
+
+    public byte[] getUUID() {
+        return uuid;
+    }
+
+    public void setUnireg(byte[] unireg) {
+        this.unireg = unireg;
+    }
+
+    public void setUUID(UUID rowUuid) {
+        this.uuid = ByteBuffer.allocate(16)
+                .putLong(rowUuid.getMostSignificantBits())
+                .putLong(rowUuid.getLeastSignificantBits())
+                .array();
+    }
+}
