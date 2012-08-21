1309a563513a34cdb42ba650fc622009eb40e7a6
John Edstrom <jedstrom@nearinfinity.com>
2012-08-21 13:00:44 -0400

    Fixed bugs with merge, all functionality should be available now

--- HBaseAdapter/src/main/java/com/nearinfinity/mysqlengine/scanner/DoubleResultScanner.java
+++ HBaseAdapter/src/main/java/com/nearinfinity/mysqlengine/scanner/DoubleResultScanner.java
@@ -7,6 +7,8 @@ import com.nearinfinity.hbaseclient.strategy.PrimaryIndexScanStrategy;
 import com.nearinfinity.hbaseclient.strategy.ScanStrategy;
 import org.apache.hadoop.hbase.client.Result;
 import org.apache.hadoop.hbase.client.ResultScanner;
+import org.apache.hadoop.hbase.util.Bytes;
+import org.apache.log4j.Logger;
 
 import java.io.IOException;
 import java.util.Arrays;
@@ -26,6 +28,8 @@ public class DoubleResultScanner implements HBaseResultScanner {
     private final HBaseClient client;
     private ResultScanner primaryScanner;
 
+    private static final Logger logger = Logger.getLogger(DoubleResultScanner.class);
+
     public DoubleResultScanner(ResultScanner scanner, String tableName, String columnName, HBaseClient client) {
         this.secondaryScanner = scanner;
         this.tableName = tableName;
@@ -38,30 +42,39 @@ public class DoubleResultScanner implements HBaseResultScanner {
     @Override
     public Result next(byte[] valueToSkip) throws IOException {
         if (this.primaryScanner == null) {
+            logger.info("Primary scanner is null, searching secondary to instantiate");
             Result result = this.secondaryScanner.next();
             if (result == null) {
                 return null;
             }
+
             byte[] value = ResultParser.parseValue(result);
+
+            logger.info("Parsed value from the result is " + Bytes.toStringBinary(value));
+
             if (valueToSkip != null && Arrays.equals(value, valueToSkip)) {
+                logger.info("Value matched valueToSkip");
                 Result nextResult = secondaryScanner.next();
                 if (nextResult == null) {
                     return null;
                 }
                 value = ResultParser.parseValue(nextResult);
+                logger.info("Reset parsed value to " + Bytes.toString(value));
             }
-
+            logger.info("Starting primary scanner with value " + Bytes.toStringBinary(value));
             ScanStrategy strategy = new PrimaryIndexScanStrategy(tableName, columnName, value);
             primaryScanner = client.getScanner(strategy);
         }
 
         Result result = primaryScanner.next();
         while (result == null) {
+            logger.info("Primary scanner returned a null result, going on to secondary");
             Result secondaryResult = secondaryScanner.next();
             if (secondaryResult == null) {
                 return null;
             }
             byte[] value = ResultParser.parseValue(secondaryResult);
+            logger.info("Next value from secondary is " + Bytes.toStringBinary(value));
             ScanStrategy strategy = new PrimaryIndexScanStrategy(tableName, columnName, value);
             primaryScanner = client.getScanner(strategy);
             result = primaryScanner.next();
