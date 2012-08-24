29ae8501d9e9267d801a560d354722c49626c333
Dan Burkert <dburkert@nearinfinity.com>
2012-08-24 10:47:57 -0400

    Fix type in numeric.inc string.inc and temporal.inc

--- bin/run-tests.sh
+++ bin/run-tests.sh
@@ -1,2 +1,2 @@
 cd $MYSQL_HOME/mysql-test
-./mtr --suite=cloud-test --extern socket=/tmp/mysql.sock --force --retry=0
+./mtr --suite=cloud-test --extern socket=/tmp/mysql.sock --force --retry=0 --max-test-fail=100
