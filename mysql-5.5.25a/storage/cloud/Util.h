aca51949682f424585bd003a1473a0245a4eac75
acrute <acrute@nearinfinity.com>
2012-08-29 13:22:32 -0400

    dates and times should be properly indexed now

--- mysql-5.5.25a/storage/cloud/Util.h
+++ mysql-5.5.25a/storage/cloud/Util.h
@@ -4,6 +4,7 @@
 #include <jni.h>
 
 #include "Macros.h"
+#include "m_string.h"
 
 enum hbase_data_type { UNKNOWN_TYPE, JAVA_STRING, JAVA_LONG, JAVA_ULONG, JAVA_DOUBLE, JAVA_TIME, JAVA_DATE, JAVA_DATETIME };
 
@@ -11,4 +12,9 @@ hbase_data_type extract_field_type(Field *field);
 bool is_unsigned_field(Field *field);
 jclass find_jni_class(const char* class_name, JNIEnv* env);
 void print_java_exception(JNIEnv* jni_env);
+void extract_mysql_newdate(long tmp, MYSQL_TIME *time);
+void extract_mysql_old_date(int tmp, MYSQL_TIME *time);
+void extract_mysql_time(long tmp, MYSQL_TIME *time);
+void extract_mysql_datetime(ulonglong tmp, MYSQL_TIME *time);
+
 #endif
