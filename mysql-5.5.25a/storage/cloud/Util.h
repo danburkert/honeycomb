7da8c70efe0fefcd81334e33312d601b85dcfa88
Dan Burkert <dburkert@nearinfinity.com>
2012-08-31 13:17:32 -0400

    remove remaining uses of JAVA_ types in favor of mysql types.

--- mysql-5.5.25a/storage/cloud/Util.h
+++ mysql-5.5.25a/storage/cloud/Util.h
@@ -11,9 +11,6 @@
 #include "Macros.h"
 #include "m_string.h"
 
-enum hbase_data_type { UNKNOWN_TYPE, JAVA_STRING, JAVA_LONG, JAVA_ULONG, JAVA_DOUBLE, JAVA_TIME, JAVA_DATE, JAVA_DATETIME, JAVA_BINARY };
-
-hbase_data_type extract_field_type(Field *field);
 bool is_unsigned_field(Field *field);
 jclass find_jni_class(const char* class_name, JNIEnv* env);
 void print_java_exception(JNIEnv* jni_env);
@@ -22,6 +19,5 @@ void extract_mysql_old_date(int32 tmp, MYSQL_TIME *time);
 void extract_mysql_time(long tmp, MYSQL_TIME *time);
 void extract_mysql_datetime(longlong tmp, MYSQL_TIME *time);
 void extract_mysql_timestamp(long tmp, MYSQL_TIME *time, THD *thd);
-enum enum_mysql_timestamp_type timestamp_type_of_mysql_type(enum enum_field_types t);
 
 #endif
