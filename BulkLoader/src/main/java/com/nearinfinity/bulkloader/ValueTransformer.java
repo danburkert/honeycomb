09c2599960f8fbf78f2185c085cfd879d916f803
Dan Burkert <dburkert@nearinfinity.com>
2012-09-07 15:43:48 -0400

    Add special handling in the bulk loader for time and decimal

--- BulkLoader/src/main/java/com/nearinfinity/bulkloader/ValueTransformer.java
+++ BulkLoader/src/main/java/com/nearinfinity/bulkloader/ValueTransformer.java
@@ -1,25 +1,91 @@
 package com.nearinfinity.bulkloader;
 
 import com.nearinfinity.hbaseclient.ColumnType;
+import org.apache.commons.lang3.time.DateUtils;
+import org.apache.hadoop.hbase.util.Bytes;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.MathContext;
+import java.math.RoundingMode;
 import java.nio.ByteBuffer;
+import java.text.DateFormat;
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class ValueTransformer {
 
-    public static byte[] transform(String val, ColumnType t) {
+    public static byte[] transform(String val, ColumnType t) throws Exception {
         byte[] ret = null;
         switch (t) {
             case LONG:
-            case ULONG:
                 ret = ByteBuffer.allocate(8).putLong(Long.parseLong(val)).array();
+                break;
+            case ULONG:
+                BigInteger n = new BigInteger(val);
+                if(n.compareTo(BigInteger.ZERO) == -1) {
+                    throw new Exception("negative value provided for unsigned column.  value: ".concat(val));
+                }
+                ret = ByteBuffer.allocate(8).putLong(new BigInteger(val).longValue()).array();
                 break;
             case DOUBLE:
                 ret = ByteBuffer.allocate(8).putDouble(Double.parseDouble(val)).array();
                 break;
-            case TIME:
             case DATE:
+                Date d = DateUtils.parseDateStrictly(val,
+                        "yyyy-MM-dd",
+                        "yyyy/MM/dd",
+                        "yyyy.MM.dd",
+                        "yyyyMMdd");
+                SimpleDateFormat time_formatter = new SimpleDateFormat("yyyy-MM-dd");
+                ret = time_formatter.format(d).getBytes();
+                break;
+            case TIME:
+                d = DateUtils.parseDateStrictly(val,
+                        "HH:mm:ss",
+                        "HHmmss");
+                time_formatter = new SimpleDateFormat("HH:mm:ss");
+                ret = time_formatter.format(d).getBytes();
+                break;
             case DATETIME:
+                d = DateUtils.parseDateStrictly(val,
+                        "yyyy-MM-dd HH:mm:ss",
+                        "yyyy/MM/dd HH:mm:ss",
+                        "yyyy.MM.dd HH:mm:ss",
+                        "yyyyMMdd HHmmss");
+                time_formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                ret = time_formatter.format(d).getBytes();
+                break;
             case DECIMAL:
+                int precision = 5;
+                int right_scale = 2;
+                int left_scale = precision - 2;
+                BigDecimal x = new BigDecimal(val);
+                boolean is_negative = x.compareTo(BigDecimal.ZERO) == -1;
+                x = x.abs();
+                BigDecimal left = x.setScale(0, RoundingMode.DOWN);
+                BigDecimal right = x.subtract(left).movePointRight(x.scale());
+                int right_bytes_len = bytesFromDigits(right_scale);
+                int left_bytes_len = bytesFromDigits(left_scale);
+                byte[] left_bytes = left.toBigInteger().toByteArray();
+                byte[] right_bytes = right.toBigInteger().toByteArray();
+                // Bit twiddling is fun
+                byte[] buff = new byte[left_bytes_len + right_bytes_len];
+                for (int i = 0; i < left_bytes.length; i++ ) {
+                    buff[i + left_bytes_len - left_bytes.length] = left_bytes[i];
+                }
+                for (int i = 0; i < right_bytes.length; i++ ) {
+                    buff[i + right_bytes_len - right_bytes.length + left_bytes_len] = right_bytes[i];
+                }
+                buff[0] ^= -128; // Flip first bit, 0x80
+                if (is_negative) { // Flip all bits
+                    for (int i = 0; i < buff.length; i++) {
+                        buff[i] ^= -1; // 0xff
+                    }
+                }
+                ret = buff;
+                break;
             case STRING:
             case BINARY:
             case NONE:
@@ -27,6 +93,13 @@ public class ValueTransformer {
                 ret = val.getBytes();
                 break;
         }
+        return ret;
+    }
+
+    public static int bytesFromDigits(int digits) {
+        int ret = 0;
+        ret += 4 * (digits / 9);
+        ret += (digits % 9 + 1) / 2;
         return ret;
     }
 }
