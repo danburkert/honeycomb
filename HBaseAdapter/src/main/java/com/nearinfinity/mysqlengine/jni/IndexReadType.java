980237cb373e01ee5091d9842d61906fa59b0a54
Sean <showell@nearinfinity.com>
2012-09-20 11:01:52 -0400

    Optimized imports and removed more templates.

--- HBaseAdapter/src/main/java/com/nearinfinity/mysqlengine/jni/IndexReadType.java
+++ HBaseAdapter/src/main/java/com/nearinfinity/mysqlengine/jni/IndexReadType.java
@@ -1,12 +1,5 @@
 package com.nearinfinity.mysqlengine.jni;
 
-/**
- * Created with IntelliJ IDEA.
- * User: jedstrom
- * Date: 8/15/12
- * Time: 10:55 AM
- * To change this template use File | Settings | File Templates.
- */
 public enum IndexReadType {
     HA_READ_KEY_EXACT,
     HA_READ_AFTER_KEY,
