0facf480c044f0dd974a4eae2a21a3c6d11b889d
Sean <showell@nearinfinity.com>
2012-10-04 14:24:34 -0400

    Changed the load incremental hfile to a sequential loading. Added calculations for bulk loading data.

--- BulkLoader/src/main/java/com/nearinfinity/bulkloader/SamplingReducer.java
+++ BulkLoader/src/main/java/com/nearinfinity/bulkloader/SamplingReducer.java
@@ -1,31 +1,46 @@
 package com.nearinfinity.bulkloader;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
 import org.apache.hadoop.hbase.mapreduce.TableReducer;
+import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.hadoop.io.Writable;
 
 import java.io.IOException;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+import static java.lang.String.format;
 
 public class SamplingReducer extends TableReducer<ImmutableBytesWritable, Put, Writable> {
-    private long regionSize;
-    private static final long _1GB = 1 << 30;
+    private long regionSize, hfilesExpected;
+    public static final long _1GB = 1 << 30;
     public static final byte[] DUMMY_FAMILY = "dummy".getBytes();
+    private static final Log LOG = LogFactory.getLog(SamplingReducer.class);
+
 
     @Override
     protected void setup(Context context) throws IOException {
         Configuration conf = context.getConfiguration();
-        long hbaseRegionSize = conf.getLong("hbase.hregion.max.filesize", _1GB);
-        regionSize = Math.min(conf.getLong("region_size", _1GB), hbaseRegionSize);
+        double samplePercent = Double.parseDouble(conf.get("sample_percent"));
+        regionSize = Math.round(samplePercent * conf.getLong("hbase.hregion.max.filesize", _1GB));
+        hfilesExpected = conf.getLong("hfiles_expected", 10);
     }
 
     @Override
     public void reduce(ImmutableBytesWritable key, Iterable<Put> rowKeys, Context context) throws IOException, InterruptedException {
         // Row keys can only be iterated over *once*
-        Set<byte[]> splits = createSplits(rowKeys, regionSize);
+        List<byte[]> splits = createSplits(rowKeys, regionSize);
+
+        int size = splits.size();
+        LOG.info(format("Splits created: %d/Expected splits %d", size, hfilesExpected));
+        if (size < hfilesExpected) {
+            addExtraSplits(splits);
+        }
 
         for (byte[] split : splits) {
             Put put = new Put(split).add(DUMMY_FAMILY, "spacer".getBytes(), new byte[0]);
@@ -33,10 +48,57 @@ public class SamplingReducer extends TableReducer<ImmutableBytesWritable, Put, W
         }
     }
 
-    public static Set<byte[]> createSplits(Iterable<Put> rowKeys, long regionSize) {
+    private void addExtraSplits(List<byte[]> splits) {
+        long neededSplits = hfilesExpected - splits.size();
+        LOG.info(format("Adding %d extra splits to the split list.", neededSplits));
+        List<byte[]> additionalSplits;
+        additionalSplits = new LinkedList<byte[]>();
+        while (splits.size() < hfilesExpected) {
+            addSplits(splits.iterator(), additionalSplits, neededSplits);
+            splits.addAll(additionalSplits);
+            additionalSplits.clear();
+        }
+
+        LOG.info(format("Total splits %d", splits.size()));
+    }
+
+    public static void addSplits(Iterator<byte[]> splitList, List<byte[]> additionalSplits, long neededSplits) {
+        if (neededSplits == 0) {
+            return;
+        }
+
+        if (!splitList.hasNext()) {
+            return;
+        }
+
+        byte[] first = splitList.next();
+        if (!splitList.hasNext()) {
+            return;
+        }
+
+        byte[] second = splitList.next();
+        byte[][] split;
+        try {
+            if (Bytes.compareTo(first, second) < 0) {
+                split = Bytes.split(first, second, true, 1);
+            } else {
+                split = Bytes.split(second, first, true, 1);
+            }
+        } catch (IllegalArgumentException e) {
+            LOG.warn(String.format("Could not find split between %s and %s", Bytes.toStringBinary(first), Bytes.toStringBinary(second)));
+            addSplits(splitList, additionalSplits, neededSplits - 1);
+            return;
+        }
+
+        byte[] differenceKey = split[1];
+        additionalSplits.add(differenceKey);
+        addSplits(splitList, additionalSplits, neededSplits - 1);
+    }
+
+    public static List<byte[]> createSplits(Iterable<Put> rowKeys, long regionSize) {
         long putSize = 0;
         boolean first = true;
-        Set<byte[]> splits = new HashSet<byte[]>();
+        List<byte[]> splits = new LinkedList<byte[]>();
         Put firstPut = new Put();
         for (Put put : rowKeys) {
             if (first) {
