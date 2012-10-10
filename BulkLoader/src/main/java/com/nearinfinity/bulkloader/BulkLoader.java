92c4fb318fc515ae896da95d8973b9e8db232a89
Sean <showell@nearinfinity.com>
2012-10-10 12:55:15 -0400

    Bulk load is finally working for 5 million rows.

--- BulkLoader/src/main/java/com/nearinfinity/bulkloader/BulkLoader.java
+++ BulkLoader/src/main/java/com/nearinfinity/bulkloader/BulkLoader.java
@@ -12,12 +12,10 @@ import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.ContentSummary;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.hbase.*;
+import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.client.*;
 import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
 import org.apache.hadoop.hbase.mapreduce.HFileOutputFormat;
-import org.apache.hadoop.hbase.mapreduce.LoadIncrementalHFiles;
-import org.apache.hadoop.hbase.mapreduce.PutSortReducer;
 import org.apache.hadoop.hbase.mapreduce.TableMapReduceUtil;
 import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.hadoop.io.Text;
@@ -39,6 +37,8 @@ import static java.lang.String.format;
 
 public class BulkLoader extends Configured implements Tool {
     private static final Log LOG = LogFactory.getLog(BulkLoader.class);
+
+    public static final String TableInfoPath = "table_info";
 
     public enum Counters {ROWS, FAILED_ROWS}
 
@@ -70,7 +70,7 @@ public class BulkLoader extends Configured implements Tool {
         return PutListFactory.createPutList(valueMap, tableInfo);
     }
 
-    public static TableInfo extractTableInfo(Configuration conf) throws IOException {
+    private static TableInfo getTableInfo(Configuration conf) throws IOException {
         String zkQuorum = conf.get("zk_quorum");
         String sqlTableName = conf.get("sql_table_name");
         String hbaseTableName = conf.get("hb_table");
@@ -78,6 +78,12 @@ public class BulkLoader extends Configured implements Tool {
         HBaseClient client = new HBaseClient(hbaseTableName, zkQuorum);
 
         return client.getTableInfo(sqlTableName);
+    }
+
+    public static TableInfo extractTableInfo(Configuration conf) throws IOException {
+        TableInfo info = new TableInfo("Dummy", 0);
+        info.read(conf.get(TableInfoPath));
+        return info;
     }
 
     @Override
@@ -96,141 +102,15 @@ public class BulkLoader extends Configured implements Tool {
 
         updateConfiguration(conf, args, params);
 
-        HBaseAdmin admin = new HBaseAdmin(conf);
-
-        HBaseAdminColumn.addDummyFamily(admin);
-
-        createSamplingJob(conf);
-
-        createSplits(conf, admin);
-
-        createBulkLoadJob(conf);
-
-        deleteDummyData(conf);
-
-        HBaseAdminColumn.deleteDummyFamily(admin);
+        LoadStrategy loadStrategy = new LoadStrategy(conf);
+        loadStrategy.load();
 
         return 0;
     }
 
-    private static void printQuorum(Job job) {
-        LOG.info(format("*** Using quorum %s ***", job.getConfiguration().get("hbase.zookeeper.quorum")));
-    }
-
-    private static void createSamplingJob(Configuration conf) throws IOException, ClassNotFoundException, InterruptedException {
-        LOG.info("Setting up the sampling job");
-        String outputPath = conf.get("output_path");
-        String inputPath = conf.get("input_path");
-        int columnCount = conf.getInt(SamplingPartitioner.COLUMN_COUNT, 3);
-        Job job = new Job(conf, "Sample data in " + inputPath);
-        job.setJarByClass(SamplingMapper.class);
-
-        FileInputFormat.setInputPaths(job, new Path(inputPath));
-        job.setInputFormatClass(TextInputFormat.class);
-
-        job.setMapperClass(SamplingMapper.class);
-
-        job.setMapOutputKeyClass(ImmutableBytesWritable.class);
-        job.setMapOutputValueClass(Put.class);
-        FileOutputFormat.setOutputPath(job, new Path(outputPath));
-        job.setNumReduceTasks(2 * columnCount + 1);
-        TableMapReduceUtil.initTableReducerJob(conf.get("hb_table"), SamplingReducer.class, job, SamplingPartitioner.class);
-        printQuorum(job);
-        job.waitForCompletion(true);
-        deletePath(conf, outputPath);
-    }
-
-    private static void createBulkLoadJob(Configuration conf) throws Exception {
-        String outputPath = conf.get("output_path");
-        String inputPath = conf.get("input_path");
-        HTable table = new HTable(conf, conf.get("hb_table"));
-
-        Job job = new Job(conf, "Import from file " + inputPath + " into table " + conf.get("hb_table"));
-
-        job.setJarByClass(BulkLoader.class);
-        FileInputFormat.setInputPaths(job, new Path(inputPath));
-        job.setInputFormatClass(TextInputFormat.class);
-
-        job.setMapperClass(BulkLoaderMapper.class);
-        job.setMapOutputKeyClass(ImmutableBytesWritable.class);
-        job.setMapOutputValueClass(Put.class);
-
-        job.setReducerClass(PutSortReducer.class);
-        Path outputDir = new Path(outputPath);
-        FileOutputFormat.setOutputPath(job, outputDir);
-
-        HFileOutputFormat.configureIncrementalLoad(job, table);
-
-        printQuorum(job);
-        if (job.waitForCompletion(true)) {
-            SequentialLoadIncrementalHFiles fileLoader = new SequentialLoadIncrementalHFiles(conf);
-            fileLoader.doBulkLoad(outputDir, table);
-            long count = job.getCounters().findCounter(Counters.ROWS).getValue();
-            HBaseClient client = new HBaseClient(conf.get("hb_table"), conf.get("zk_quorum"));
-            client.incrementRowCount(conf.get("sql_table_name"), count);
-        } else {
-            LOG.error("*** Bulk load job failed during run. Not loading data into HBase.***");
-        }
-
-        deletePath(conf, outputPath);
-    }
-
-    private static void deleteDummyData(Configuration conf) throws IOException {
-        HTable table = new HTable(conf, conf.get("hb_table"));
-        ResultScanner splitScanner = table.getScanner("dummy".getBytes());
-        List<Delete> deleteList = new LinkedList<Delete>();
-        for (Result result : splitScanner) {
-            deleteList.add(new Delete(result.getRow()));
-        }
-
-        table.delete(deleteList);
-    }
-
-    private static void createSplits(Configuration conf, HBaseAdmin admin) throws IOException {
-        String tableName = conf.get("hb_table");
-        HTable table = new HTable(conf, tableName);
-        ResultScanner splitScanner = table.getScanner("dummy".getBytes());
-        List<byte[]> retrySplits = new LinkedList<byte[]>();
-        for (Result result : splitScanner) {
-            try {
-                admin.split(tableName.getBytes(), result.getRow());
-            } catch (Exception e) {
-                retrySplits.add(result.getRow());
-                LOG.warn("Split " + Bytes.toStringBinary(result.getRow()) + " threw exception");
-            }
-        }
-
-        if (!retrySplits.isEmpty()) {
-            retryFailedSplits(admin, tableName, retrySplits);
-        }
-    }
-
-    private static void retryFailedSplits(HBaseAdmin admin, String tableName, List<byte[]> retrySplits) {
-        int retryCount = 3;
-        for (int x = 0; x < retrySplits.size(); x++) {
-            try {
-                LOG.info("Retrying split " + Bytes.toStringBinary(retrySplits.get(x)));
-                admin.split(tableName.getBytes(), retrySplits.get(x));
-            } catch (Exception e) {
-                retryCount--;
-                if (retryCount == 0) {
-                    LOG.info(format("Skipping %s too many failed retries.", Bytes.toStringBinary(retrySplits.get(x))));
-                    retryCount = 3;
-                } else {
-                    x--;
-                    try {
-                        Thread.sleep(2000);
-                    } catch (InterruptedException ignored) {
-                    }
-                }
-            }
-        }
-    }
-
-    private static void deletePath(Configuration conf, String path) throws IOException {
-        FileSystem fileSystem = FileSystem.get(conf);
-        fileSystem.delete(new Path(path), true);
-        fileSystem.close();
+    private static void setupTableInfo(Configuration conf) throws IOException {
+        TableInfo info = getTableInfo(conf);
+        conf.set(TableInfoPath, info.write());
     }
 
     private static void updateConfiguration(Configuration conf, String[] args, Map<String, String> params) throws IOException {
@@ -253,6 +133,7 @@ public class BulkLoader extends Configured implements Tool {
 
         SamplingPartitioner.setColumnCount(conf, columnCount);
         updateDataInfo(conf, inputPath);
+        setupTableInfo(conf);
     }
 
     private static void updateDataInfo(Configuration conf, String inputPath) throws IOException {
