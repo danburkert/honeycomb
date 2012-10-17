8e27f8139aabf73b5278425812c9bf7f5a069690
Sean <showell@nearinfinity.com>
2012-10-17 16:03:59 -0400

    Saving place

--- HBaseAdapter/src/main/java/com/nearinfinity/hbaseclient/strategy/NullScanStrategy.java
+++ HBaseAdapter/src/main/java/com/nearinfinity/hbaseclient/strategy/NullScanStrategy.java
@@ -6,13 +6,6 @@ import com.nearinfinity.hbaseclient.ScanFactory;
 import com.nearinfinity.hbaseclient.TableInfo;
 import org.apache.hadoop.hbase.client.Scan;
 
-/**
- * Created with IntelliJ IDEA.
- * User: jedstrom
- * Date: 8/21/12
- * Time: 8:47 AM
- * To change this template use File | Settings | File Templates.
- */
 public class NullScanStrategy extends ScanStrategyBase {
     public NullScanStrategy(String tableName, String columnName, byte[] value) {
         super(tableName, columnName, value);
--- HBaseAdapter/src/main/java/com/nearinfinity/hbaseclient/strategy/ScanStrategyBase.java
+++ HBaseAdapter/src/main/java/com/nearinfinity/hbaseclient/strategy/ScanStrategyBase.java
@@ -4,12 +4,14 @@ import com.nearinfinity.hbaseclient.ScanFactory;
 import com.nearinfinity.hbaseclient.TableInfo;
 import org.apache.hadoop.hbase.client.Scan;
 
+import java.util.List;
+
 public class ScanStrategyBase implements ScanStrategy {
     protected String tableName;
-    protected String columnName;
+    protected List<String> columnName;
     protected byte[] value;
 
-    public ScanStrategyBase(String tableName, String columnName, byte[] value) {
+    public ScanStrategyBase(String tableName, List<String> columnName, byte[] value) {
         this.tableName = tableName;
         this.columnName = columnName;
         this.value = value;
