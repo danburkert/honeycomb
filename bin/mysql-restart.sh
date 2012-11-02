dde9c3366feceeab11b58d3137d60ae0ff472fc8
Sean <showell@nearinfinity.com>
2012-11-02 15:13:00 -0400

    Commenting out the incrementRowCount fixes the float test. All tests pass. See issue #26.

--- bin/mysql-restart.sh
+++ bin/mysql-restart.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
+REMOVE_PLUGIN="UNINSTALL PLUGIN cloud;"
+INSTALL_PLUGIN="INSTALL PLUGIN cloud SONAME 'ha_cloud.so';"
+
+echo $REMOVE_PLUGIN > remove_plugin.sql
+echo $INSTALL_PLUGIN > install_plugin.sql
 
 sudo mysql.server stop && sudo mysql.server start --debug=d:t:L:F:o,/tmp/mysqld.trace
+
+echo "removing plugin"
+mysql -u root < remove_plugin.sql
+echo "installing plugin"
+mysql -u root < install_plugin.sql
+
+rm remove_plugin.sql
+rm install_plugin.sql
