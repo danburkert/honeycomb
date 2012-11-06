8a9c432057bb1ecb8fbb145eef4967e7d85ae43a
Dan Burkert <danburkert@gmail.com>
2012-11-06 18:30:50 -0500

    make MedianRegionSplitPolicy class

---
+++ MedianRegionSplitPolicy/src/main/java/com/nearinfinity/MedianRegionSplitPolicy/MedianRegionSplitPolicy.java
@@ -0,0 +1,15 @@
+package com.nearinfinity.MedianRegionSplitPolicy;
+
+import org.apache.hadoop.hbase.regionserver.RegionSplitPolicy;
+
+/**
+ * Hello world!
+ *
+ */
+public class MedianRegionSplitPolicy extends RegionSplitPolicy
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
