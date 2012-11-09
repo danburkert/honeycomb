65f1c4d38248c130405dbc6251e4ba1f2a55e512
Dan Burkert <danburkert@gmail.com>
2012-11-09 11:19:11 -0500

    rename and refactor MedianSplitPolicy

--- MedianSplitPolicy/src/test/java/org/apache/hadoop/hbase/regionserver/MedianSplitPolicyTest.java
+++ MedianSplitPolicy/src/test/java/org/apache/hadoop/hbase/regionserver/MedianSplitPolicyTest.java
@@ -1,66 +1,48 @@
 package org.apache.hadoop.hbase.regionserver;
 
 import junit.framework.TestCase;
-import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.hadoop.hbase.util.Pair;
-import org.apache.hadoop.hbase.regionserver.MedianRegionSplitPolicy;
 
-import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-/**
- * Created with IntelliJ IDEA.
- * User: dburkert
- * Date: 11/7/12
- * Time: 2:11 PM
- * To change this template use File | Settings | File Templates.
- */
-public class MedianRegionSplitPolicyTest extends TestCase {
-    public void setUp() throws Exception {
-
-    }
-
-    public void tearDown() throws Exception {
-
-    }
+public class MedianSplitPolicyTest extends TestCase {
 
     public void testCombineSamples() {
         List<Pair<byte[], Long>> samples = new ArrayList<Pair<byte[], Long>>();
-        byte[] weighted_median;
+        byte[] combined;
         byte[] expected;
 
         // (0xff, 10) -> 0xff
         samples.clear();
         samples.add(Pair.newPair(new byte[]{(byte) 0xff}, 10l));
-        weighted_median = MedianRegionSplitPolicy.combineSamples(samples);
+        combined = MedianSplitPolicy.combineSamples(samples);
         expected = new byte[]{(byte) 0xff};
-        assertTrue(Arrays.equals(expected, weighted_median));
+        assertTrue(Arrays.equals(expected, combined));
 
         // ("aa", 1), ("cc", 1) -> "bb"
         samples.clear();
         samples.add(Pair.newPair("aa".getBytes(), 1l));
         samples.add(Pair.newPair("cc".getBytes(), 1l));
-        weighted_median = MedianRegionSplitPolicy.combineSamples(samples);
+        combined = MedianSplitPolicy.combineSamples(samples);
         expected = "bb".getBytes();
-        assertTrue(Arrays.equals(expected, weighted_median));
+        assertTrue(Arrays.equals(expected, combined));
 
         // ("aa", 1), ("dd", 2) -> "cc"
         samples.clear();
         samples.add(Pair.newPair("aa".getBytes(), 1l));
         samples.add(Pair.newPair("dd".getBytes(), 2l));
-        weighted_median = MedianRegionSplitPolicy.combineSamples(samples);
+        combined = MedianSplitPolicy.combineSamples(samples);
         expected = "cc".getBytes();
-        assertTrue(Arrays.equals(expected, weighted_median));
+        assertTrue(Arrays.equals(expected, combined));
 
         // ("aaa", 1), ("aa", 1) -> "aa0"
         samples.clear();
         samples.add(Pair.newPair("aaa".getBytes(), 1l));
         samples.add(Pair.newPair("aa".getBytes(), 1l));
-        weighted_median = MedianRegionSplitPolicy.combineSamples(samples);
+        combined = MedianSplitPolicy.combineSamples(samples);
         expected = "aa0".getBytes();
-        assertTrue(Arrays.equals(expected, weighted_median));
+        assertTrue(Arrays.equals(expected, combined));
     }
-
 }
