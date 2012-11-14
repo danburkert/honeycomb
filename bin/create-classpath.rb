e4fa1713b65acfd90f2a858fdefe65954aa0bd50
Sean <showell@nearinfinity.com>
2012-11-14 13:59:58 -0500

    Script to generate class paths.

---
+++ bin/create-classpath.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+
+paths = []
+ARGV.each do |dir|
+  jar_glob = File.join(dir, "**/*.jar")
+  Dir.glob(jar_glob).each do | file |
+    paths << File.expand_path(file) 
+  end
+end
+
+print paths.join(":")
