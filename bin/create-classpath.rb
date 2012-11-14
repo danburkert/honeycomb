f78f4af1c3d4a6e22368b558c57c24b4576f244d
Dan Burkert <danburkert@gmail.com>
2012-11-14 14:30:31 -0500

    refine create-classpath script

--- bin/create-classpath.rb
+++ bin/create-classpath.rb
@@ -1,7 +1,8 @@
 #!/usr/bin/env ruby
 
 paths = []
-ARGV.each do |dir|
+dirs = [ENV['HADOOP_HOME'], ENV['HBASE_HOME'], "#{ENV['M2_HOME']}/repository/com/nearinfinity"]
+dirs.each do |dir|
   jar_glob = File.join(dir, "**/*.jar")
   Dir.glob(jar_glob).each do | file |
     paths << File.expand_path(file) 
