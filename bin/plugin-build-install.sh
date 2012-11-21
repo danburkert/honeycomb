807dd0b0d6e1555dc0b2421d54791573b5e6a4d6
Sean <showell@nearinfinity.com>
2012-11-21 16:56:54 -0500

    Added support for linux. Updated read me.

--- bin/plugin-build-install.sh
+++ bin/plugin-build-install.sh
@@ -24,24 +24,42 @@ echo "Running make in $build_dir"
 make
 if [ ! -d $MYSQL_HOME ]
 then
-  echo "Installing and setting up mysql (Currently Mac only)"
+  echo "Installing and setting up mysql."
   sudo make install
-  sudo dscl . create /Groups/mysql
-  sudo dscl . create /Groups/mysql gid 296
-  sudo dscl . -create /Users/mysql
-  sudo dscl . append /Groups/mysql GroupMembership mysql
+  platform=$(uname)
+  if [[ "$platform" == "Darwin" ]]; then
+    echo "Creating mysql Mac User"
+    sudo dscl . -create /Groups/mysql
+    sudo dscl . -create /Groups/mysql gid 296
+    sudo dscl . -create /Users/mysql
+    sudo dscl . -create /Users/mysql UserShell /usr/bin/false
+    sudo dscl . -create /Users/mysql NFSHomeDirectory /var/empty
+    sudo dscl . -append /Groups/mysql GroupMembership mysql
+  elif [[ "$platform" == "Linux" ]]; then
+    echo "Creating mysql Linux User"
+    sudo groupadd mysql
+    sudo useradd -r -g mysql mysql
+  fi
+
   sudo chown -R mysql $MYSQL_HOME/data
   sudo chgrp -R mysql $MYSQL_HOME/data
-
-  sudo $MYSQL_HOME/scripts/mysql_install_db --user=mysql
+  echo "Creating grant tables"
+  pushd $MYSQL_HOME
+  sudo scripts/mysql_install_db --user=mysql
   echo "Starting up MySQL"
-  sudo $MYSQL_HOME/support-files/mysql.server start
+  sudo support-files/mysql.server start
+  popd
 fi
 
 link=$MYSQL_HOME/lib/plugin/ha_cloud.so 
 target=$HONEYCOMB_HOME/build/storage/cloud/ha_cloud.so
 if [ ! -h $link ]
 then
+  if [ -e $link ]; then
+    echo "Changing file to symbolic link"
+    sudo rm $link
+  fi
+
   echo "Creating a symbolic link from $link to $target"
   sudo ln -s $target $link
 fi
