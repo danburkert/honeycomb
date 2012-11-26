cf10fa663ab58a4a04649843b972c61918ed7085
Dan Burkert <danburkert@gmail.com>
2012-11-26 15:32:17 -0500

    80 char column

--- bin/create-classpath.rb
+++ bin/create-classpath.rb
@@ -4,7 +4,7 @@ paths = []
 ARGV.each do |dir|
   jar_glob = File.join(dir, "**/*.jar")
   Dir.glob(jar_glob).each do | file |
-    paths << File.expand_path(file) 
+    paths << File.expand_path(file)
   end
 end
 
