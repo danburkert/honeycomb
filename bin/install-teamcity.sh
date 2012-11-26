26fd6192b9eb4c62f36c79accef995f55d2e308e
Sean <showell@nearinfinity.com>
2012-11-26 13:58:16 -0500

    Beefed up the team city install script.

---
+++ bin/install-teamcity.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+honeycomb_lib=/usr/local/lib/honeycomb
+plugin_path=/usr/lib64/mysql/plugin/ha_cloud.so 
+tests_path=/usr/share/mysql-test/suite/cloud-test
+function test_link
+{
+  path=$1
+  [ -L $path ] || { echo "Link $path is missing. Can't continue installing."; exit 1; }
+}
+
+[ -d $honeycomb_lib && -w $honeycomb_lib ] || { echo "$honeycomb_lib is required, and has to be writable."; exit 1; }
+test_link $plugin_path
+test_link $tests_path
+
+if [[ "$(find $honeycomb_lib -type d -empty)" == "" ]]
+then
+  echo "Removing old code from $honeycomb_lib"
+  rm -rf $honeycomb_lib/*
+fi
+
+echo "Copying ha_cloud.so, mysqlengine.jar and cloud-test to $honeycomb_lib"
+cp -R cloud/cloud-test $honeycomb_lib
+cp build/storage/cloud/ha_cloud.so $honeycomb_lib
+cp HBaseAdapter/target/lib/*.jar $honeycomb_lib
+cp HBaseAdapter/target/mysqlengine-0.1.jar $honeycomb_lib
+chmod a+x $honeycomb_lib/*.jar
