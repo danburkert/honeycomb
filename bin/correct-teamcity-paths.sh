dc7341089d6dddc0a3dc21bfbf4fd9b9fa1a4735
Sean <showell@nearinfinity.com>
2012-11-27 10:18:11 -0500

    Forcing overwrites.

--- bin/correct-teamcity-paths.sh
+++ bin/correct-teamcity-paths.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 if [ -e pom.apache.xml ]; then
-  mv pom.apache.xml pom.xml
+  mv -f pom.apache.xml pom.xml
 fi
 
 if [ -d cloud ]; then
-  mv cloud mysql-5.5.28/storage/
+  mv -f cloud mysql-5.5.28/storage/
 fi
