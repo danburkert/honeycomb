d87f122636e76ed44e6973d36520f14fd00f948d
Dan Burkert <danburkert@gmail.com>
2012-11-27 18:38:09 -0500

    intermediate: fixing funcs tests

--- bin/run-funcs-tests.sh
+++ bin/run-funcs-tests.sh
@@ -3,10 +3,10 @@
 : ${MYSQL_HOME?"Need to set MYSQL_HOME environmental variable."}
 
 cd $MYSQL_HOME/mysql-test/
-rm suite/cloud-test/funcs/r/*.reject
+#rm suite/cloud-test/funcs/r/*.reject
 ./mtr --suite=cloud-test/funcs             \
   --mysqld=--plugin-load=cloud=ha_cloud.so \
   --mysqld=--default-storage-engine=cloud  \
   --mysqld=--character-set-server=utf8     \
   --mysqld=--collation-server=utf8_bin     \
-  --force --retry=0 --max-test-fail=10
+  --force --retry=0 --max-test-fail=400
