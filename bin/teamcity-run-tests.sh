a83108e363aa953faa88e15de0c722d2c784a5e4
Sean <showell@nearinfinity.com>
2012-11-27 14:25:46 -0500

    Removing the teamcity specific test runner.

--- bin/teamcity-run-tests.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-: ${MYSQL_HOME?"Need to set MYSQL_HOME environmental variable."}
-cd $MYSQL_HOME/mysql-test
-rm suite/cloud-test/r/*.reject
-./mtr --suite=cloud-test --extern socket=/var/lib/mysql/mysql.sock --force --retry=2 --max-test-fail=10 --vardir=/tmp/var/tests
