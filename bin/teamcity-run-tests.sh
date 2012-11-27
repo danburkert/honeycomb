7f8301c19c9e6e09ea9a4764f12407db3c8311b6
Sean <showell@nearinfinity.com>
2012-11-27 10:37:00 -0500

    Changed to a host for the mysql tests.

--- bin/teamcity-run-tests.sh
+++ bin/teamcity-run-tests.sh
@@ -3,4 +3,4 @@
 : ${MYSQL_HOME?"Need to set MYSQL_HOME environmental variable."}
 cd $MYSQL_HOME/mysql-test
 rm suite/cloud-test/r/*.reject
-./mtr --suite=cloud-test --extern socket=/lib/mysql/mysql.sock --force --retry=2 --max-test-fail=10
+./mtr --suite=cloud-test --extern host=nic-hadoop-r310-01 --force --retry=2 --max-test-fail=10
