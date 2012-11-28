5e45bdf64a3342b07c0a505f918625eea4128b9a
Sean <showell@nearinfinity.com>
2012-11-28 11:47:38 -0500

    Removed the sshaskpass.

--- bin/teamcity-puppet-binaries.sh
+++ bin/teamcity-puppet-binaries.sh
@@ -2,8 +2,6 @@
 
 bundle=`pwd`/honeycomb.tar.gz
 honeycomb_lib=/usr/local/lib/honeycomb
-askpass=sshaskpass.sh
-command -v $askpass >/dev/null 2>&1 || { echo >&2 "$askpass is required to run $0."; exit 1; }
 
 [ -d $honeycomb_lib ] || { echo "$honeycomb_lib must exist to use this script."; exit 1; }
 
@@ -15,6 +13,6 @@ popd
 password="password"
 puppet=localadmin@nic-hadoop-puppet
 echo "Sending the tar off to puppet"
-echo $password | $askpass scp $bundle $puppet:/opt/packages
+scp $bundle $puppet:/opt/packages
 echo "Extracting the tar on puppet"
-echo $password | $askpass ssh $puppet "cd /opt/packages; tar xzf $bundle"
+ssh $puppet "cd /opt/packages; tar xzf $bundle"
