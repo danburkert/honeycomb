e6386efebe18cf27238a66fb478eff0c92bded92
Sean <showell@nearinfinity.com>
2012-11-28 16:48:10 -0500

    Puppet distribution up and running.

--- bin/teamcity-puppet-binaries.sh
+++ bin/teamcity-puppet-binaries.sh
@@ -6,13 +6,10 @@ honeycomb_lib=/usr/local/lib/honeycomb
 [ -d $honeycomb_lib ] || { echo "$honeycomb_lib must exist to use this script."; exit 1; }
 
 pushd $honeycomb_lib
-echo "Creating the tar for honeycomb"
+echo "Creating the tar for honeycomb in $bundle"
 tar -czf $bundle --exclude='cloud-test' .
 popd
 
-password="password"
 puppet=localadmin@nic-hadoop-puppet
 echo "Sending the tar off to puppet"
 scp $bundle $puppet:/opt/packages
-echo "Extracting the tar on puppet"
-ssh $puppet "cd /opt/packages; tar xzf $bundle"
