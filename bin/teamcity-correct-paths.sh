988023393a1b9053c8eefcb6bf6ec3a30cbff545
Sean <showell@nearinfinity.com>
2012-11-30 10:50:38 -0500

    Updated the correct paths for client side checkout.

--- bin/teamcity-correct-paths.sh
+++ bin/teamcity-correct-paths.sh
@@ -1,16 +1,6 @@
 #!/bin/sh
 
-if [ -e pom.apache.xml ]; then
-  mv -f pom.cdh.xml pom.xml
+if [ -h pom.xml ]; then
+  rm pom.xml
+  ln -s pom.cdh.xml pom.xml
 fi
-
-dest=mysql-5.5.28/storage/cloud
-if [ -d cloud ]; then
-  if [ -e $dest ]; then
-    echo "Removing $dest"
-    rm $dest
-  fi
-
-  mv -f cloud $dest
-fi
-
