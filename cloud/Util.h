9839aff567f06a8752dc39d2f46514843c13c7c8
Sean <showell@nearinfinity.com>
2012-12-03 11:46:02 -0500

    Added namespacing to the sql tables stored in HBase.

--- cloud/Util.h
+++ cloud/Util.h
@@ -14,7 +14,7 @@ bool is_unsigned_field(Field *field);
 void reverse_bytes(uchar *begin, uint length);
 bool is_little_endian();
 void make_big_endian(uchar *begin, uint length);
-const char *extract_table_name_from_path(const char *path);
+char *extract_table_name_from_path(const char *path);
 uchar* create_key_copy(Field* index_field, const uchar* key, uint* key_len, THD* thd);
 
 #endif
