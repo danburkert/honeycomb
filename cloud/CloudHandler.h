3b015282cfbb9366740f5d27250e0481fa77e22a
Sean <showell@nearinfinity.com>
2012-12-17 14:07:44 -0500

    Removed start/end bulk delete (now using external lock).

--- cloud/CloudHandler.h
+++ cloud/CloudHandler.h
@@ -51,8 +51,6 @@ class CloudHandler : public handler
     void java_to_sql(uchar *buf, jobject row_map);
     jobject sql_to_java();
     int delete_all_rows();
-    bool start_bulk_delete();
-    int end_bulk_delete();
     int delete_table(const char *name);
     void drop_table(const char *name);
     int truncate();
