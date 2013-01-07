eb9a41a2938f9d100e96b110b611e6adc9c702a0
Dan Burkert <danburkert@gmail.com>
2013-01-07 15:05:01 -0500

    first draft of new performance test script

---
+++ bin/run-performance-tests.rb
@@ -0,0 +1,41 @@
+#!/usr/bin/ruby
+
+### Globals FTW
+@build_folder = "build"
+@host = "nic-hadoop-smmc07"
+@db = 'person'
+@tables = ['inno_05', 'hc_05',
+          'inno_06', 'hc_06',
+          'inno_07', 'hc_07']
+
+### QUERIES
+
+def countQuery(table)
+  "SELECT COUNT(*) FROM #{table};"
+end
+
+### RUN
+
+def printBench(query, concurrency, iterations)
+  `#{@build_folder}/client/mysqlslap --delimiter=";" --create="" --query="#{query}" --concurrency=#{concurrency} --iterations=#{iterations} --no-create --no-drop --create-schema=#{@db} --host=#{@host} --only-print`
+end
+
+def runBench(query, concurrency, iterations)
+  `#{@build_folder}/client/mysqlslap --delimiter=";" --create="" --query="#{query}" --concurrency=#{concurrency} --iterations=#{iterations} --no-create --no-drop --create-schema=#{@db} --host=#{@host}`
+end
+
+def runTests
+  @tables.each do |table|
+    puts("Table " + table + ":")
+    puts runBench(countQuery(table), 10, 3)
+  end
+end
+
+def printTests
+  @tables.each do |table|
+    puts("Table " + table + ":")
+    puts printBench(countQuery(table), 10, 3)
+  end
+end
+
+printTests
