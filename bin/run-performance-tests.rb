b025f4dfbb7690ca56c3fca1fa9d27d6dccb964d
Dan Burkert <danburkert@gmail.com>
2013-01-07 17:00:39 -0500

    analyze performance tests by default

--- bin/run-performance-tests.rb
+++ bin/run-performance-tests.rb
@@ -25,18 +25,18 @@ def analyzeBench(query, concurrency, iterations)
   `./mysqlslap --delimiter=";" --create="" --query="#{query}" --concurrency=#{concurrency} --iterations=#{iterations} --no-create --no-drop --create-schema=#{@db} --host=#{@host} --user="root" | ./analyze-slap.r`
 end
 
-def runTests
-  @tables.each do |table|
-    puts("Table " + table + ":")
-    puts runBench(countQuery(table), 10, 3)
-  end
-end
-
-def printTests
+def runBenchmarks
   @tables.each do |table|
     puts("Table " + table + ":")
     puts analyzeBench(countQuery(table), 10, 3)
   end
 end
 
-runTests
+def printBenchmarks
+  @tables.each do |table|
+    puts("Table " + table + ":")
+    puts printBench(countQuery(table), 10, 3)
+  end
+end
+
+runBenchmarks
