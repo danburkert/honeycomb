10f719ae060e69fede19eec697afe5720865fcce
Sean Howell <showell@nearinfinity.com>
2013-01-07 14:37:41 -0500

    Apparently build numbers don't come in as numbers.

--- bin/analyze-tests.R
+++ bin/analyze-tests.R
@@ -10,7 +10,7 @@ pdf(file=output)
 layout(matrix(c(1, 1, 2, 3), 2, 2, byrow=T))
 for(name in colnames(rows)) { 
   values <- rows[name][,]
-  plot(x, values, type="b", xlab="build", ylab=name, main=sprintf("%s (build %d)", name, build_number))
+  plot(x, values, type="b", xlab="build", ylab=name, main=sprintf("%s (build %s)", name, build_number))
   summary_info <- capture.output(summary(values))
   summary_info[1] <- paste(summary_info[1], "   StdDev")
   summary_info[2] <- paste(summary_info[2], "   ", signif(sd(values, na.rm=T)))
