6a23f5292347a8257b9f0057d7f31b76fb408671
Sean Howell <showell@nearinfinity.com>
2013-01-08 09:04:17 -0500

    Renamed test scripts to be consistent with no file ending.

--- bin/teamcity-result-stats.sh
+++ bin/teamcity-result-stats.sh
@@ -3,7 +3,7 @@
 test_name=$1
 build_number=$2
 output=/tmp/$test_name.$build_number.out
-./run-$test_name.sh | tee $output
+./run-$test_name | tee $output
 ruby ./parse-test-output.rb $output /tmp/$test_name.csv
 rm $output
 Rscript ./analyze-tests.R /tmp/$test_name.csv /tmp/$test_name.pdf $build_number
