7e27c3e7085c39f005c3c0d6436ad5cc6750b1e8
Dan Burkert <danburkert@gmail.com>
2013-01-14 11:31:34 -0500

    Add CLI to benchmark runner

---
+++ benchmark/example-options.clj
@@ -0,0 +1,6 @@
+{:tables [:hc_05 :inno_05]
+ :warmup 5
+ :clients [2 4 8]
+ :queries [point-name range-salary]
+ :append false
+ :out "out.csv"}
--- benchmark/src/benchmark/core.clj
+++ benchmark/src/benchmark/core.clj
@@ -1,15 +1,10 @@
 (ns benchmark.core
   (:require [clojure.java.jdbc :as sql]
             [clojure.string :as s]
-            [benchmark.query :as q]))
-
-(defmacro time-call
-  "Evaluates expr and returns the time it took in seconds."
-  [expr]
-  `(let [start# (. System (nanoTime))]
-     (do
-       ~expr
-       (/ (double (- (. System (nanoTime)) start#)) 1000000000.0))))
+            [clojure.java.io :as io]
+            [benchmark.query :as q]
+            [clojure.tools.cli :as cli])
+  (:gen-class))
 
 (defn client
   "Simulated client.  Opens a connection to db and executes stmts on a separate
@@ -31,19 +26,61 @@
           :stop (map #(/ (- % (first times)) 1000000000.0) times))))))
 
 (defn benchmark
-  [db-spec table query-fn num-clients warmup-p bench-p]
+  "Run individual benchmark and return results."
+  [db-spec table query-fn num-clients warmup bench]
   (let [phase (atom :warmup)
         clients (doall (for [_ (range num-clients)]
                          (client db-spec table query-fn phase)))]
     (do
-      (Thread/sleep (* warmup-p 1000))
+      (Thread/sleep (* warmup 1000))
       (reset! phase :bench)
-      (Thread/sleep (* bench-p 1000))
+      (Thread/sleep (* bench 1000))
       (reset! phase :stop)
       (map deref clients))))
 
-(def db-spec {:classname "com.mysql.jdbc.Driver"
-         :subprotocol "mysql"
-         :subname "//localhost:3306/hbase"
-         :user "root"
-         :password ""})
+(defn benchmark-suite
+  "Run benchmarks against different configurations of tables, number of
+   concurrent client connections, and query type.  The database, warmup
+   period, benchmark period, and output are consistent across runs."
+  [tables queries clients db-spec warmup bench]
+  (dorun
+    (for [table tables
+          query queries
+          clients clients]
+      (println (name table) bench (name query) clients))))
+
+(defn -main [& args]
+  (let [[cli-opts _ banner]
+        (cli/cli args
+                 ["-h" "--help" "Show this menu" :flag true :default false]
+                 ["-t" "--tables" "Tables to run benchmarks against.  e.g. \"[:hc_05 :inno_05]\". Required." :parse-fn read-string]
+                 ["-q" "--queries" "Query types to benchmark.  eg. \"[point-name range-salary]\". Required." :parse-fn read-string]
+                 ["-d" "--db" "Database connection spec." :parse-fn read-string :default {:classname "com.mysql.jdbc.Driver"
+                                                                                          :subprotocol "mysql"
+                                                                                          :subname "//localhost:3306/hbase"
+                                                                                          :user "root"}]
+                 ["-w" "--warmup" "Warmup period in seconds." :default 10]
+                 ["-b" "--bench" "Benchmark period in seconds." :default 30]
+                 ["-c" "--clients" "Number of concurrent client connections.  Accepts multiple values for multiple runs." :default [5] :parse-fn read-string]
+                 ["-o" "--out" "Path to file where results will be appended.  Defaults to writing output to stdout."]
+                 ["-a" "--append" "Append to output file instead of overwrite." :flag true :default true]
+                 ["--options" "Path to options file which contains an options map. File options override cli options."])
+        opts (if (:options cli-opts)
+               (merge cli-opts (read-string (slurp (:options cli-opts))))
+               cli-opts)]
+    (when (or (:help opts) (not (and (:tables opts) (:queries opts))))
+      (do (println banner) #_(System/exit 0)))
+    (let [queries (map (partial get (ns-publics 'benchmark.query)) (:queries opts))
+          run-benchmarks #(benchmark-suite (:tables opts)
+                                           queries
+                                           (:clients opts)
+                                           (:db opts)
+                                           (:warmup opts)
+                                           (:bench opts))]
+      (if (:out opts)
+        (with-open [writer (io/writer (:out opts) :append (:append opts))]
+          (binding [*out* writer
+                    *flush-on-newline* false]
+            (run-benchmarks)))
+        (run-benchmarks))
+      (shutdown-agents))))
