d6f226573dee6bdd4e514c0fc667ee9d20855675
Dan Burkert <danburkert@gmail.com>
2013-01-16 12:42:01 -0500

    change output name of benchmark results

--- benchmark/buildserver-options.clj
+++ benchmark/buildserver-options.clj
@@ -4,7 +4,7 @@
  :clients [1 2 4 8 16 32]
  :queries [range-salary point-name]
  :append false
- :out "bench.ssv"
+ :out "benchmark.ssv"
  :db {:classname "com.mysql.jdbc.Driver"
       :subprotocol "mysql"
       :subname "//nic-hadoop-smmc07.nearinfinity.com:3306/person"
