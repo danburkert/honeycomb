bff5f11a4b5d2861c8ee56dc3970602cb616bf6a
Sean Howell <showell@nearinfinity.com>
2013-01-18 15:50:41 -0500

    Removed include for file that doesn't exist.

--- honeycomb/HoneycombHandler.h
+++ honeycomb/HoneycombHandler.h
@@ -11,7 +11,6 @@
 #include "Logging.h"
 #include "Java.h"
 #include "Util.h"
-#include "ScopedJavaResource.h"
 
 #include "my_global.h"          /* ulonglong */
 #include "thr_lock.h"           /* THR_LOCK, THR_LOCK_DATA */
