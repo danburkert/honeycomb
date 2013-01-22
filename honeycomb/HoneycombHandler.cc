4467712b678c35dc33f4ecd815924c19307303c2
Dan Burkert <danburkert@gmail.com>
2013-01-22 11:03:05 -0500

    redo Sean"s changes in 8237cd5 on this branch

--- honeycomb/HoneycombHandler.cc
+++ honeycomb/HoneycombHandler.cc
@@ -15,8 +15,8 @@ HoneycombHandler::HoneycombHandler(handlerton *hton, TABLE_SHARE *table_arg,
 : handler(hton, table_arg), jvm(jvm), honeycomb_mutex(mutex),
   honeycomb_open_tables(open_tables), hbase_adapter(NULL)
 {
+  attach_thread();
   this->ref_length = 16;
-  this->initialize_adapter();
   this->rows_written = 0;
   this->failed_key_index = 0;
   this->scan_ids_length = 32;
@@ -24,7 +24,6 @@ HoneycombHandler::HoneycombHandler(handlerton *hton, TABLE_SHARE *table_arg,
   this->scan_ids = new long long[this->scan_ids_length];
   this->curr_write_id = -1;
   memset(scan_ids, 0, scan_ids_length);
-  attach_thread();
 }
 
 HoneycombHandler::~HoneycombHandler()
