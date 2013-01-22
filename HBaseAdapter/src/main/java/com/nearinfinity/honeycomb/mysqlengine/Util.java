7f98c711ed3ee118054276f274bdf4cdc49af026
Sean Howell <showell@nearinfinity.com>
2013-01-22 15:26:53 -0500

    Merged the 3 conf files into one xml file. Using libxml2 to parse the file instead of by hand.

--- HBaseAdapter/src/main/java/com/nearinfinity/honeycomb/mysqlengine/Util.java
+++ HBaseAdapter/src/main/java/com/nearinfinity/honeycomb/mysqlengine/Util.java
@@ -1,22 +1,30 @@
 package com.nearinfinity.honeycomb.mysqlengine;
 
 import org.apache.hadoop.conf.Configuration;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.io.IOException;
 
 public class Util {
     public static Configuration readConfiguration(File source)
-            throws FileNotFoundException {
-        Scanner scanner = new Scanner(source);
+            throws IOException, ParserConfigurationException, SAXException {
         Configuration params = new Configuration(false);
-        while (scanner.hasNextLine()) {
-            Scanner line = new Scanner(scanner.nextLine());
-            params.set(line.next(), line.next());
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document doc = builder.parse(source);
+        NodeList options = doc.getElementsByTagName("adapteroption");
+        for (int i = 0; i < options.getLength(); i++) {
+            Element node = (Element) options.item(i);
+            params.set(node.getAttribute("name"), node.getNodeValue());
         }
+
         return params;
     }
 }
