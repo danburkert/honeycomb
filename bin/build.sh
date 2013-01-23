cca255e5645f677ddace82ab8b448ace7cb01867
Sean Howell <showell@nearinfinity.com>
2013-01-23 17:06:05 -0500

    Added initial setup environment check. /bin/sh -> /bin/bash. mvn build install now copies over honeycomb.xml.

--- bin/build.sh
+++ bin/build.sh
@@ -1,7 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
 : ${HONEYCOMB_HOME?"Need to set HONEYCOMB_HOME environmental variable to the top of the project."}
 
+$HONEYCOMB_HOME/bin/initial-setup-check.sh
 $HONEYCOMB_HOME/bin/mvn-build-install.sh
 $HONEYCOMB_HOME/bin/plugin-build-install.sh
 $HONEYCOMB_HOME/bin/mysql-restart.sh
--- bin/mvn-build-install.sh
+++ bin/mvn-build-install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 : ${HONEYCOMB_HOME?"Need to set HONEYCOMB_HOME environmental variable to the top of the project."}
 command -v mvn >/dev/null 2>&1 || { echo >&2 "mvn is required to run $0."; exit 1; }
@@ -23,7 +23,7 @@ create_classpath=$($HONEYCOMB_HOME/bin/create-classpath.rb $honeycomb_lib $honey
 echo $create_classpath | sudo tee /etc/mysql/classpath.conf > /dev/null
 
 conf_path=/etc/mysql
-adapter_conf=$conf_path/adapter.conf
+adapter_conf=$conf_path/honeycomb.xml
 if [ ! -d $conf_path ]
 then
   echo "Creating configuration path $conf_path"
@@ -32,12 +32,7 @@ fi
 
 if [ ! -e $adapter_conf ]
 then
-  echo "Creating the adapter.conf from the repository."
-  sudo cp $HONEYCOMB_HOME/HBaseAdapter/adapter.conf $adapter_conf
-
-  _zk_quorum=localhost
-  read -p "Setup Zookeeper Quorum [$_zk_quorum]:" zk_quorum
-  zk_quorum=${zk_quorum:-$_zk_quorum}
-  sudo perl -pi -w -e "s/^zk_quorum (\w)$/zk_quorum $zk_quorum/g;" $adapter_conf
+  echo "Creating the honeycomb.xml from the repository."
+  sudo cp $HONEYCOMB_HOME/honeycomb/honeycomb-example.xml $adapter_conf
 fi
 echo "*** Don't forget to restart MySQL. The JVM doesn't autoreload the jar from the disk. ***"
