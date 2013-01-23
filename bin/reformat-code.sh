234ce9a664dadca5f493d18842d3936b0a0fde48
Sean Howell <showell@nearinfinity.com>
2013-01-23 11:34:30 -0500

    Moved the parameters for astyle out so it can be used in a git pre-commit hook.

--- bin/reformat-code.sh
+++ bin/reformat-code.sh
@@ -3,5 +3,6 @@
 : ${HONEYCOMB_HOME?"Need to set HONEYCOMB_HOME environmental variable to the top of the project."}
 command -v astyle >/dev/null 2>&1 || { echo >&2 "astyle is required to run $0."; exit 1; }
 
+parameters=$(cat astyle.parameters)
 cd $HONEYCOMB_HOME/honeycomb
-astyle -A2 -s2 -n -K -p -xd -j -c -S -C --align-pointer=type --align-reference=type *.cc *.h
+astyle $parameters *.cc *.h
