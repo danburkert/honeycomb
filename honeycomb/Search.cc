danburkert/honeycomb

commit e988f7fd9bb4bdae2e0a41fb505654f2e0a4bc33

remove remaining GetMethodID calls from HoneycombHandler

honeycomb/Search.cc
@@ -199,12 +199,10 @@ int HoneycombHandler::get_index_row(const char* indexType, uchar* buf)
 int HoneycombHandler::read_index_row(jobject index_row, uchar* buf)
 {
   jclass index_row_class = cache->index_row().clazz;
-  jmethodID get_uuid_method = this->env->GetMethodID(index_row_class, "getUUID",
-      "()[B");
-  jmethodID get_rowmap_method = this->env->GetMethodID(index_row_class,
-      "getRowMap", "()Ljava/util/Map;");
+  jmethodID get_uuid_method = cache->index_row().get_uuid;
+  jmethodID get_row_map_method = cache->index_row().get_row_map;
 
-  jobject rowMap = this->env->CallObjectMethod(index_row, get_rowmap_method);
+  jobject rowMap = this->env->CallObjectMethod(index_row, get_row_map_method);
   if (rowMap == NULL)
   {
     this->table->status = STATUS_NOT_FOUND;
@@ -240,8 +238,7 @@ int HoneycombHandler::rnd_pos(uchar *buf, uchar *pos)
       this->curr_scan_id, uuid);
 
   jclass row_class = cache->row().clazz;
-  jmethodID get_row_map_method = this->env->GetMethodID(row_class, "getRowMap",
-      "()Ljava/util/Map;");
+  jmethodID get_row_map_method = cache->index_row().get_row_map;
 
   jobject row_map = this->env->CallObjectMethod(row, get_row_map_method);
 
@@ -335,10 +332,8 @@ int HoneycombHandler::rnd_next(uchar *buf)
       next_row_method, this->curr_scan_id);
 
   jclass row_class = cache->row().clazz;
-  jmethodID get_row_map_method = this->env->GetMethodID(row_class, "getRowMap",
-      "()Ljava/util/Map;");
-  jmethodID get_uuid_method = this->env->GetMethodID(row_class, "getUUID",
-      "()[B");
+  jmethodID get_uuid_method = cache->index_row().get_uuid;
+  jmethodID get_row_map_method = cache->index_row().get_row_map;
 
   jobject row_map = this->env->CallObjectMethod(row, get_row_map_method);
 

commit 008eb9d61e3b5c2cec1951287995d8349b90fe56

fix regression introduced in e988f7fd

honeycomb/Search.cc
@@ -233,7 +233,7 @@ int HoneycombHandler::rnd_pos(uchar *buf, uchar *pos)
       this->curr_scan_id, uuid);
 
   jclass row_class = cache->row().clazz;
-  jmethodID get_row_map_method = cache->index_row().get_row_map;
+  jmethodID get_row_map_method = cache->row().get_row_map;
 
   jobject row_map = this->env->CallObjectMethod(row, get_row_map_method);
 
@@ -327,8 +327,8 @@ int HoneycombHandler::rnd_next(uchar *buf)
       next_row_method, this->curr_scan_id);
 
   jclass row_class = cache->row().clazz;
-  jmethodID get_uuid_method = cache->index_row().get_uuid;
-  jmethodID get_row_map_method = cache->index_row().get_row_map;
+  jmethodID get_uuid_method = cache->row().get_uuid;
+  jmethodID get_row_map_method = cache->row().get_row_map;
 
   jobject row_map = this->env->CallObjectMethod(row, get_row_map_method);
 