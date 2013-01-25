danburkert/honeycomb

commit 45c43488aa2ab49280ae60305f9b29031a596b25

remove java_map_insert from Java.cc

honeycomb/Crud.cc
@@ -116,7 +116,7 @@ int HoneycombHandler::create(const char *path, TABLE *table_arg,
     jobject java_metadata_obj = metadata.get_field_metadata(field, table_arg,
         create_info->auto_increment_value);
     jstring jfield_name = string_to_java_string(field->field_name);
-    java_map_insert(columnMap, jfield_name, java_metadata_obj, this->env);
+    env->CallObjectMethod(columnMap, cache->tree_map().put, jfield_name, java_metadata_obj);
   }
 
   jmethodID create_table_method = cache->hbase_adapter().create_table;
@@ -200,7 +200,7 @@ int HoneycombHandler::write_row(uchar* buf, jobject updated_fields)
 
     if (is_null)
     {
-      java_map_insert(java_row_map, field_name, NULL, this->env);
+      env->CallObjectMethod(java_row_map, cache->tree_map().put, field_name, NULL);
       continue;
     }
 
@@ -304,13 +304,13 @@ int HoneycombHandler::write_row(uchar* buf, jobject updated_fields)
     jbyteArray java_bytes = convert_value_to_java_bytes(byte_val,
         actualFieldSize, this->env);
     MY_FREE(byte_val);
-    java_map_insert(java_row_map, field_name, java_bytes, this->env);
+    env->CallObjectMethod(java_row_map, cache->tree_map().put, field_name, java_bytes);
 
     // Remember this field for later if we find that it has a unique index,
     // need to check it
     if (this->field_has_unique_index(field))
     {
-      java_map_insert(unique_values_map, field_name, java_bytes, this->env);
+      env->CallObjectMethod(java_row_map, cache->tree_map().put, field_name, java_bytes);
     }
   }
 

honeycomb/Java.cc
@@ -6,14 +6,6 @@
 #define MAP_CLASS "java/util/TreeMap"
 #define LIST_CLASS "java/util/LinkedList"
 
-jobject java_map_insert(jobject java_map, jobject key, jobject value, JNIEnv* env)
-{
-  jclass map_class = env->FindClass(MAP_CLASS);
-  jmethodID put_method = env->GetMethodID(map_class, "put", "(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;");
-
-  return env->CallObjectMethod(java_map, put_method, key, value);
-}
-
 jbyteArray java_map_get(jobject java_map, jstring key, JNIEnv* env)
 {
   jclass map_class = env->FindClass(MAP_CLASS);

honeycomb/Java.h
@@ -6,7 +6,6 @@
 #include "my_base.h"
 #include "JNICache.h"
 
-jobject java_map_insert(jobject java_map, jobject key, jobject value, JNIEnv* env);
 jbyteArray java_map_get(jobject java_map, jstring key, JNIEnv* env);
 jboolean java_map_is_empty(jobject java_map, JNIEnv* env);
 

commit 9fdeb190d46e3a877b061be4e6cfccc9309b3b2a

fix regression introduced in 45c4348

honeycomb/Crud.cc
@@ -310,7 +310,7 @@ int HoneycombHandler::write_row(uchar* buf, jobject updated_fields)
     // need to check it
     if (this->field_has_unique_index(field))
     {
-      env->CallObjectMethod(java_row_map, cache->tree_map().put, field_name, java_bytes);
+      env->CallObjectMethod(unique_values_map, cache->tree_map().put, field_name, java_bytes);
     }
   }
 