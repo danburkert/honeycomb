252f15ff7d4b6365f8a6163d49fff4d1398d2a4f
Sean Howell <showell@nearinfinity.com>
2013-02-05 09:41:35 -0500

    Updated comments on option parser.

--- honeycomb/OptionParser.h
+++ honeycomb/OptionParser.h
@@ -8,21 +8,21 @@ typedef struct st_optionparser OptionParser;
  *
  * @param filename Path to xml configuration file
  *
- * @return Options in file
+ * @return Parser for file
  */
 OptionParser* new_parser(const char* filename);
 
 /**
  * @brief Release resources held by options.
  *
- * @param parser parser
+ * @param parser Option parser
  */
 void free_parser(OptionParser* parser);
 
 /**
  * @brief Retrieve the JNI options found in the file.
  *
- * @param parser parser
+ * @param parser Option parser
  *
  * @return JNI options
  */
@@ -31,7 +31,7 @@ JavaVMOption* get_options(OptionParser* parser);
 /**
  * @brief Retrieve the number of options found in the file.
  *
- * @param parser parser
+ * @param parser Option parser
  *
  * @return Number of options found
  */
@@ -40,7 +40,7 @@ unsigned int get_optioncount(OptionParser* parser);
 /**
  * @brief Retrieves the error message from reading. Returns NULL if there was no error.
  *
- * @param parser parser
+ * @param parser Option parser
  *
  * @return Error during reading
  */
@@ -49,7 +49,7 @@ char* get_errormessage(OptionParser* parser);
 /**
  * @brief Describes whether there was an error while trying to read the options.
  *
- * @param parser parser
+ * @param parser Option parser
  *
  * @return Was an error during reading
  */
