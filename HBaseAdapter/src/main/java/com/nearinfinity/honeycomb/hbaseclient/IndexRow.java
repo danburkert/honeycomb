d8a934eaf89f5c99778aa1aacc28949fee9aa722
Sean Howell <showell@nearinfinity.com>
2013-02-15 11:16:35 -0500

    Added metrics to avro parse result.

--- HBaseAdapter/src/main/java/com/nearinfinity/honeycomb/hbaseclient/IndexRow.java
+++ HBaseAdapter/src/main/java/com/nearinfinity/honeycomb/hbaseclient/IndexRow.java
@@ -1,12 +1,8 @@
 package com.nearinfinity.honeycomb.hbaseclient;
 
-import org.apache.avro.io.Decoder;
-import org.apache.avro.io.DecoderFactory;
-import org.apache.avro.specific.SpecificDatumReader;
 import org.apache.hadoop.hbase.client.Result;
 import org.apache.log4j.Logger;
 
-import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.util.Map;
 import java.util.TreeMap;
@@ -15,9 +11,9 @@ import java.util.UUID;
 import static com.google.common.base.Preconditions.checkNotNull;
 
 public class IndexRow {
+    private static final Logger logger = Logger.getLogger(IndexRow.class);
     private byte[] uuid;
     private TreeMap<String, byte[]> rowMap;
-    private static final Logger logger = Logger.getLogger(IndexRow.class);
 
 
     public IndexRow() {
@@ -32,6 +28,13 @@ public class IndexRow {
         return uuid;
     }
 
+    private void setUUID(UUID rowUuid) {
+        this.uuid = ByteBuffer.allocate(16)
+                .putLong(rowUuid.getMostSignificantBits())
+                .putLong(rowUuid.getLeastSignificantBits())
+                .array();
+    }
+
     /**
      * Extract a SQL row out of an HBase index row result.
      *
@@ -39,37 +42,12 @@ public class IndexRow {
      */
     @SuppressWarnings("unchecked")
     public void parseResult(Result result) {
+        long start = System.currentTimeMillis();
         this.setUUID(ResultParser.parseUUID(result));
         byte[] mapBytes = ResultParser.parseValueMap(result);
         checkNotNull(mapBytes, "mapBytes");
-
-
-        SpecificDatumReader<com.nearinfinity.honeycomb.mysql.Row> rowReader =
-                new SpecificDatumReader<com.nearinfinity.honeycomb.mysql.Row>(com.nearinfinity.honeycomb.mysql.Row.class);
-
-
-        Decoder decoder = DecoderFactory.get().binaryDecoder(mapBytes, null);
-        com.nearinfinity.honeycomb.mysql.Row row = null;
-        try {
-            row = rowReader.read(null, decoder);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        rowMap = new TreeMap<String, byte[]>();
-
-        for (Map.Entry<String, ByteBuffer> entry : row.getRecords().entrySet()) {
-            String s = entry.getKey();
-            byte[] b = entry.getValue().array();
-
-            rowMap.put(s, b);
-        }
-    }
-
-    private void setUUID(UUID rowUuid) {
-        this.uuid = ByteBuffer.allocate(16)
-                .putLong(rowUuid.getMostSignificantBits())
-                .putLong(rowUuid.getLeastSignificantBits())
-                .array();
+        rowMap = Util.deserializeMap(mapBytes);
+        long end = System.currentTimeMillis();
+        Metrics.getInstance().addParseResultTime(end - start);
     }
 }
