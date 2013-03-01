29f9db0aae7790e8e7ee5b9e4773daaf365f6dca
Andy Keffalas <akeffala@nearinfinity.com>
2013-03-01 12:01:27 -0500

    Modified script to use Java's jps tool to find running HBase processes, if available

--- bin/hbase-processes.sh
+++ bin/hbase-processes.sh
@@ -1,3 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
-ps ax | grep "org.apache.hadoop.hbase" | grep -v "grep" | awk '{ print $1, $(NF-1) }'
+HBASE_PACKAGE="org.apache.hadoop.hbase"
+
+# Set the default process status command to use
+PROCESS_STATUS_CMD="ps ax"
+
+# If jps is available, use it to list running Java processes
+command -v jps >/dev/null 2>&1
+if [ $? -eq 0 ]; then
+    PROCESS_STATUS_CMD="jps -l"
+fi
+
+
+# Find all of the process ids that correspond to HBase
+$PROCESS_STATUS_CMD | grep $HBASE_PACKAGE | grep -v "grep" | awk '{ print $1, $(NF-1) }'
