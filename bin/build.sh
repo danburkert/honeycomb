1f87888c18c0b6ccd496bc936d5adf4546c6bc09
Andy Keffalas <akeffala@nearinfinity.com>
2013-03-04 13:37:48 -0500

    Perform trivial cleanup on core build script

--- bin/build.sh
+++ bin/build.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-: ${HONEYCOMB_HOME?"Need to set HONEYCOMB_HOME environmental variable to the top of the project."}
+: ${HONEYCOMB_HOME?"Need to set the HONEYCOMB_HOME environment variable to the top of the project."}
 
-$HONEYCOMB_HOME/bin/initial-setup-check.sh
-$HONEYCOMB_HOME/bin/mvn-build-install.sh
-$HONEYCOMB_HOME/bin/plugin-build-install.sh
-$HONEYCOMB_HOME/bin/mysql-restart.sh
-$HONEYCOMB_HOME/bin/install-tests.sh
+# Set the location of the build scripts
+BUILD_SCRIPTS_DIR=$HONEYCOMB_HOME/bin
+
+$BUILD_SCRIPTS_DIR/initial-setup-check.sh
+$BUILD_SCRIPTS_DIR/mvn-build-install.sh
+$BUILD_SCRIPTS_DIR/plugin-build-install.sh
+$BUILD_SCRIPTS_DIR/mysql-restart.sh
+$BUILD_SCRIPTS_DIR/install-tests.sh
