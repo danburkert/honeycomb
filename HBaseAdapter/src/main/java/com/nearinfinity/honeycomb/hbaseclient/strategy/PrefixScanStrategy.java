cc325f731d3225a35491408186a0504216f13441
Andy Keffalas <akeffala@nearinfinity.com>
2013-03-04 17:04:07 -0500

    Performed some trivial code cleanup

--- HBaseAdapter/src/main/java/com/nearinfinity/honeycomb/hbaseclient/strategy/PrefixScanStrategy.java
+++ HBaseAdapter/src/main/java/com/nearinfinity/honeycomb/hbaseclient/strategy/PrefixScanStrategy.java
@@ -1,10 +1,15 @@
 package com.nearinfinity.honeycomb.hbaseclient.strategy;
 
-import com.nearinfinity.honeycomb.hbaseclient.*;
-import org.apache.hadoop.hbase.client.Scan;
-import org.apache.hadoop.hbase.filter.PrefixFilter;
-
 import java.util.Map;
+
+import org.apache.hadoop.hbase.client.Scan;
+
+import com.nearinfinity.honeycomb.hbaseclient.Constants;
+import com.nearinfinity.honeycomb.hbaseclient.Index;
+import com.nearinfinity.honeycomb.hbaseclient.RowKeyFactory;
+import com.nearinfinity.honeycomb.hbaseclient.ScanFactory;
+import com.nearinfinity.honeycomb.hbaseclient.TableInfo;
+import com.nearinfinity.honeycomb.hbaseclient.ValueEncoder;
 
 public class PrefixScanStrategy implements ScanStrategy {
     private final ScanStrategyInfo scanInfo;
@@ -12,12 +17,13 @@ public class PrefixScanStrategy implements ScanStrategy {
     public PrefixScanStrategy(ScanStrategyInfo scanInfo) {
         this.scanInfo = scanInfo;
     }
+
     @Override
     public Scan getScan(TableInfo info) {
         long tableId = info.getId();
-        Map<String, byte[]> ascendingValueMap = ValueEncoder.correctAscendingValuePadding(info, this.scanInfo.keyValueMap(), this.scanInfo.nullSearchColumns());
-        byte[] columnId = Index.createColumnIds(this.scanInfo.columnNames(), info.columnNameToIdMap());
-        byte[] paddedValue = Index.convertToHBaseFormat(this.scanInfo.keyValueColumns(), ascendingValueMap);
+        Map<String, byte[]> ascendingValueMap = ValueEncoder.correctAscendingValuePadding(info, scanInfo.keyValueMap(), scanInfo.nullSearchColumns());
+        byte[] columnId = Index.createColumnIds(scanInfo.columnNames(), info.columnNameToIdMap());
+        byte[] paddedValue = Index.convertToHBaseFormat(scanInfo.keyValueColumns(), ascendingValueMap);
 
         byte[] startKey = RowKeyFactory.buildIndexRowKey(tableId, columnId, paddedValue, Constants.ZERO_UUID);
         byte[] endKey = RowKeyFactory.buildIndexRowKey(tableId, columnId, paddedValue, Constants.FULL_UUID);
@@ -29,6 +35,6 @@ public class PrefixScanStrategy implements ScanStrategy {
 
     @Override
     public String getTableName() {
-        return this.scanInfo.tableName();
+        return scanInfo.tableName();
     }
 }
