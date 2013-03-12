09c734b85ec79638d70238d7656273d8a1f3cb93
Andy Keffalas <akeffala@nearinfinity.com>
2013-03-12 18:53:26 -0400

    Using Bash's builtin set command to terminate the shell upon command failure

--- bin/build.sh
+++ bin/build.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
+# Exit the shell when a command exits with a non-zero value and it has not been checked
+set -e
+
 : ${HONEYCOMB_HOME?"Need to set the HONEYCOMB_HOME environment variable to the top of the project."}
 
 # Set the location of the build scripts
 BUILD_SCRIPTS_DIR=$HONEYCOMB_HOME/bin
 
 $BUILD_SCRIPTS_DIR/initial-setup-check.sh
-[ $? -ne 0 ] && { exit 1; }
 $BUILD_SCRIPTS_DIR/mvn-build-install.sh
-[ $? -ne 0 ] && { exit 1; }
 $BUILD_SCRIPTS_DIR/plugin-build-install.sh
-[ $? -ne 0 ] && { exit 1; }
 $BUILD_SCRIPTS_DIR/mysql-restart.sh
-[ $? -ne 0 ] && { exit 1; }
 $BUILD_SCRIPTS_DIR/install-tests.sh
-[ $? -ne 0 ] && { exit 1; }
