58b19afcceca41d7c8467aa690dc9c2e58b79538
Dan Burkert <danburkert@gmail.com>
2013-03-12 18:43:34 -0400

    fix test regression introduced by 9a02011fec278a0aa534000c358171e5f2b05b11

--- bin/build.sh
+++ bin/build.sh
@@ -6,7 +6,12 @@
 BUILD_SCRIPTS_DIR=$HONEYCOMB_HOME/bin
 
 $BUILD_SCRIPTS_DIR/initial-setup-check.sh
+[ $? -ne 0 ] && { exit 1; }
 $BUILD_SCRIPTS_DIR/mvn-build-install.sh
+[ $? -ne 0 ] && { exit 1; }
 $BUILD_SCRIPTS_DIR/plugin-build-install.sh
+[ $? -ne 0 ] && { exit 1; }
 $BUILD_SCRIPTS_DIR/mysql-restart.sh
+[ $? -ne 0 ] && { exit 1; }
 $BUILD_SCRIPTS_DIR/install-tests.sh
+[ $? -ne 0 ] && { exit 1; }
