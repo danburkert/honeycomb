78d81a3dc167df62646716306678e2192b06f231
Sean Howell <showell@nearinfinity.com>
2013-03-26 08:59:31 -0400

    Added documentation to JavaFrame.

--- honeycomb/JavaFrame.h
+++ honeycomb/JavaFrame.h
@@ -2,6 +2,14 @@
 #define JAVAFRAME_H
 #include <jni.h>
 
+/**
+  * @brief Creates a frame for tracking local references for JNI objects. The destructors frees JNI objects on scope exit.
+ *
+ * Pretty much any call into JNIEnv that return a non-primitive (i.e. inherits from jobject) is a local reference.
+ * Java provides a set of functions for explicitly managing the lifetime
+ * of JNI local references instead of waiting for the JVM to cleanup the objects. PushLocalFrame and PopLocalFrame are 
+ * used to save a frame of the allocated JNI objects. 
+ */
 class JavaFrame
 {
   private:
