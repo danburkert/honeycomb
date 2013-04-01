6afc3146df5fd42cfa68c47fe96a96fa864b0ff0
Andy Keffalas <akeffala@nearinfinity.com>
2013-04-01 11:21:36 -0400

    Added brief documentation and minor cleanup to PrefixRow rowkey classes

--- HBaseAdapter/src/main/java/com/nearinfinity/honeycomb/hbase/rowkey/RowsRow.java
+++ HBaseAdapter/src/main/java/com/nearinfinity/honeycomb/hbase/rowkey/RowsRow.java
@@ -1,9 +1,12 @@
 package com.nearinfinity.honeycomb.hbase.rowkey;
 
+/**
+ * Representation of the rowkey associated with the row count details for the tables being stored
+ */
 public class RowsRow extends PrefixRow {
-    private static final byte[] rowKey = {0x03};
+    private static final byte[] ROWKEY = {0x03};
 
     public RowsRow() {
-        super(rowKey);
+        super(ROWKEY);
     }
 }
