95c52af94d72faf0cfa90fe97b0591315267781e
Andy Keffalas <akeffala@nearinfinity.com>
2013-04-01 16:50:03 -0400

    Minor test cleanup and added coverage to exercise method contract

--- HBaseAdapter/src/test/java/com/nearinfinity/honeycomb/hbase/VarEncoderTest.java
+++ HBaseAdapter/src/test/java/com/nearinfinity/honeycomb/hbase/VarEncoderTest.java
@@ -1,30 +1,32 @@
 package com.nearinfinity.honeycomb.hbase;
 
-import com.google.common.primitives.UnsignedBytes;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import net.java.quickcheck.Generator;
 import net.java.quickcheck.generator.CombinedGenerators;
 import net.java.quickcheck.generator.PrimitiveGenerators;
 import net.java.quickcheck.generator.distribution.Distribution;
 import net.java.quickcheck.generator.iterable.Iterables;
+
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import com.google.common.primitives.UnsignedBytes;
 
 public class VarEncoderTest {
     // POSITIVE_NORMAL distribution gives more probability to numbers near 0.
     // This is an attempt to get a uniform distribution over the number of
     // significant bytes in the long, instead of a uniform distribution over the
     // range of the long.  It still doesn't do enough; we need an exponential distribution.
-    private Generator<Long> uLongGen = PrimitiveGenerators.longs(0, Long.MAX_VALUE, Distribution.POSITIV_NORMAL);
+    private static final Generator<Long> ULONG_GEN = PrimitiveGenerators.longs(0, Long.MAX_VALUE, Distribution.POSITIV_NORMAL);
 
-    private Generator<byte[]> bytesGen = CombinedGenerators.byteArrays();
+    private static final Generator<byte[]> BYTES_GEN = CombinedGenerators.byteArrays();
 
     @Test
     public void testULongEncDec() {
-        for (long n : Iterables.toIterable(uLongGen)) {
+        for (long n : Iterables.toIterable(ULONG_GEN)) {
             Assert.assertEquals(n, VarEncoder.decodeULong(VarEncoder.encodeULong(n)));
         }
     }
@@ -34,12 +36,22 @@ public class VarEncoderTest {
         VarEncoder.encodeULong(-1);
     }
 
+    @Test(expected = NullPointerException.class)
+    public void testDecodeBytesNullBytes() {
+        VarEncoder.decodeBytes(null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testDecodeBytesEmptyBytes() {
+        VarEncoder.decodeBytes(new byte[0]);
+    }
+
     @Test
     public void testULongEncSort() {
         List<Long> longs = new ArrayList<Long>();
         List<byte[]> bytes = new ArrayList<byte[]>();
 
-        for (long n : Iterables.toIterable(uLongGen)) {
+        for (long n : Iterables.toIterable(ULONG_GEN)) {
             longs.add(new Long(n));
             bytes.add(VarEncoder.encodeULong(n));
         }
@@ -57,7 +69,7 @@ public class VarEncoderTest {
 
     @Test
     public void testBytesEncDec() {
-        for (byte[] b : Iterables.toIterable(bytesGen)) {
+        for (byte[] b : Iterables.toIterable(BYTES_GEN)) {
             Assert.assertArrayEquals(b, VarEncoder.decodeBytes(VarEncoder.encodeBytes(b)));
         }
     }
@@ -67,7 +79,7 @@ public class VarEncoderTest {
         List<byte[]> bytes = new ArrayList<byte[]>();
         List<byte[]> encodedBytes = new ArrayList<byte[]>();
 
-        for (byte[] b : Iterables.toIterable(bytesGen)) {
+        for (byte[] b : Iterables.toIterable(BYTES_GEN)) {
             bytes.add(b);
             encodedBytes.add(VarEncoder.encodeBytes(b));
         }
