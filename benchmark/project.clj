2430b56cf985a5c0e43945420ef4a801fb635767
Dan Burkert <danburkert@gmail.com>
2013-04-11 10:11:57 -0400

    bump benchmark dependency versions

--- benchmark/project.clj
+++ benchmark/project.clj
@@ -4,8 +4,8 @@
             :license {:name "Eclipse Public License"
                       :url "http://www.eclipse.org/legal/epl-v10.html"}
             :main nearinfinity.honeycomb.benchmark.core
-            :dependencies [[org.clojure/clojure "1.5.0-RC1"]
-                           [org.clojure/java.jdbc "0.2.3"]
+            :dependencies [[org.clojure/clojure "1.5.1"]
+                           [org.clojure/java.jdbc "0.3.0-alpha1"]
                            [mysql/mysql-connector-java "5.1.6"]
                            [clojureql "1.0.4"]
                            [org.clojure/tools.cli "0.2.2"]
