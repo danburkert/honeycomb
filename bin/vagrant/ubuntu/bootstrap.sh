c98b1b6ecf4cf23fa4cc6b77acd44703ae4193ec
Dan Burkert <danburkert@gmail.com>
2013-04-17 10:27:36 -0400

    more vagrant stuff

--- bin/vagrant/ubuntu/bootstrap.sh
+++ bin/vagrant/ubuntu/bootstrap.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # Update aptitude
+sudo echo "Acquire::http::Proxy \"http://192.168.31.55:8080\";" >> /etc/apt/apt.conf.d/01proxy
 apt-get update
 
 #install oracle java 6
@@ -36,11 +37,10 @@ sudo -E -u hdfs hadoop fs -chown -R mapred /var/lib/hadoop-hdfs/cache/mapred
 sudo -E -u hdfs hadoop fs -mkdir /hbase
 sudo -E -u hdfs hadoop fs -chown hbase /hbase
 
+echo "127.0.0.1 precise64" >> /etc/hosts
 apt-get install -y hbase hbase-master hbase-regionserver
 
-#echo "export HBASE_MANAGES_ZK=true" >> /etc/hbase/conf/hbase-env.sh
 cp /vagrant/bin/vagrant/ubuntu/hbase-site.xml /etc/hbase/conf/
-echo "127.0.0.1 precise64" >> /etc/hosts
 
 zookeeper-server-initialize --myid=1
 zookeeper-server start
@@ -52,4 +52,29 @@ apt-get -y install dpkg-dev cmake maven libncurses5-dev libxml2-dev git vim
 apt-get source mysql-5.5
 rm /home/vagrant/*.tar.gz
 rm /home/vagrant/*.dsc
-#apt-get -y install mysql-5.5
+mv mysql-5.5 /usr/local/mysql-5.5
+mkdir /usr/local/mysql-5.5/build
+chown -R vagrant:vagrant /usr/local/mysql-5.5
+ln -s /vagrant/honeycomb /usr/local/mysql-5.5/storage/
+
+# Install MySQL
+sudo debconf-set-selections <<< 'mysql-server-<version> mysql-server/root_password password your_password'
+sudo debconf-set-selections <<< 'mysql-server-<version> mysql-server/root_password_again password your_password'
+sudo apt-get -y install mysql-server
+
+sudo mkdir -p /usr/local/etc/honeycomb   # Configuration
+sudo mkdir -p /var/log/honeycomb         # Logs
+sudo mkdir -p /usr/local/lib/honeycomb   # Jars
+sudo chown mysql:adm /usr/local/etc/honeycomb
+sudo chown mysql:mysql /var/log/honeycomb
+sudo chown mysql:adm /usr/local/lib/honeycomb
+
+wget http://mirror.sdunix.com/apache/avro/avro-1.7.4/c/avro-c-1.7.4.tar.gz
+tar zxf avro-c-1.7.4.tar.gz
+rm avro-c-1.7.4.tar.gz
+mv avro-c-1.7.4 /usr/local/
+mkdir /usr/local/avro-c-1.7.4/build
+cd /usr/local/avro-c-1.7.4/build
+cmake .. -DCMAKE_INSTALL_PREFIX=/usr
+make
+make install
