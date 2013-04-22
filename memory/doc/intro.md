854313cb87be4ffd0417d825d92dd92f015b8d38
Dan Burkert <danburkert@gmail.com>
2013-04-22 10:11:10 -0400

    start memory store

---
+++ memory/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to memory
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ memory/test/com/nearinfinity/honeycomb/memory_test.clj
@@ -0,0 +1,7 @@
+(ns com.nearinfinity.honeycomb.memory-test
+  (:use clojure.test
+        com.nearinfinity.honeycomb.memory))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
