a17f9c6a3978a56e73397c7daef6789eb12ab4ae
Andy Keffalas <akeffala@nearinfinity.com>
2013-04-24 13:52:21 -0400

    Replaced include with forward declarations and removed unused includes

--- honeycomb/AvroUtil.h
+++ honeycomb/AvroUtil.h
@@ -1,8 +1,11 @@
 #ifndef AVRO_UTIL_H
 #define AVRO_UTIL_H
 
-#include <avro.h>
-#include <stdlib.h>
+#include <cstdlib>
+
+struct avro_value;
+typedef avro_value avro_value_t;
+
 
 int serialize_object(avro_value_t* obj, const char** buf, size_t* len);
 
@@ -11,4 +14,5 @@ int deserialize_object(avro_value_t* obj, const char* buf, int64_t len);
 int get_map_value(avro_value_t* schema, const char* entry_key, const char* map_name, const char** value, size_t* size);
 
 int set_map_value(avro_value_t* schema, const char* entry_key, const char* map_name, char* value, size_t size);
+
 #endif
