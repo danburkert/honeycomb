3078931ecf665219f0a0abf81d57d93b3d5ec364
Dan Burkert <danburkert@gmail.com>
2013-05-15 14:14:49 -0400

    move memory into storage-engine-backends

--- storage-engine-backends/memory/project.clj
+++ storage-engine-backends/memory/project.clj
@@ -1,10 +1,11 @@
-(defproject com.nearinfinity.honeycomb/memory "0.1.0-SNAPSHOT"
-  :description "In-memory storage adaptor for the Honeycomb storage engine"
-  :url "http://example.com/FIXME"
+(defproject com.nearinfinity.honeycomb.memory/honeycomb-memory "0.1-SNAPSHOT"
+  :description "In-memory backend for the Honeycomb storage engine"
+  :url "http://www.github.com/nearinfinity/honeycomb"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  ;; NOTE: when dependencies change, run `lein pom` to regenerate the pom.xml
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [com.nearinfinity.honeycomb/mysqlengine "0.1"]
+                 [com.nearinfinity.honeycomb/honeycomb "0.1-SNAPSHOT"]
                  [com.google.guava/guava "14.0.1"]
                  [com.google.inject/guice "3.0"]
                  [com.google.inject.extensions/guice-multibindings "3.0"]]
