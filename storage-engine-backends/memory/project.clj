5b318afe49e645703382c83e64881e4f6c7aef52
Dan Burkert <danburkert@gmail.com>
2013-05-16 10:00:36 -0400

    add pom to memory adapter

--- storage-engine-backends/memory/project.clj
+++ storage-engine-backends/memory/project.clj
@@ -1,8 +1,8 @@
 (defproject com.nearinfinity.honeycomb.memory/honeycomb-memory "0.1-SNAPSHOT"
   :description "In-memory backend for the Honeycomb storage engine"
   :url "http://www.github.com/nearinfinity/honeycomb"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "Apache License, Version 2.0"
+            :url "https://www.apache.org/licenses/LICENSE-2.0.html"}
   ;; NOTE: when dependencies change, run `lein pom` to regenerate the pom.xml
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [com.nearinfinity.honeycomb/honeycomb "0.1-SNAPSHOT"]
@@ -10,4 +10,5 @@
                  [com.google.inject/guice "3.0"]
                  [com.google.inject.extensions/guice-multibindings "3.0"]]
   :global-vars {*warn-on-reflection* true}
-  :aot :all)
+  :aot :all
+  :parent  [com.nearinfinity.honeycomb/honeycomb-parent "0.1-SNAPSHOT" :relative-path "../../pom.xml"])
