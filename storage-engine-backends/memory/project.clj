92970001ae32d961dcbca2f7241781aba4961fe6
Dan Burkert <danburkert@gmail.com>
2013-05-17 11:27:54 -0400

    Add maven clojure plugin for building memory via mvn

--- storage-engine-backends/memory/project.clj
+++ storage-engine-backends/memory/project.clj
@@ -3,12 +3,10 @@
   :url "http://www.github.com/nearinfinity/honeycomb"
   :license {:name "Apache License, Version 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0.html"}
-  ;; NOTE: when dependencies change, run `lein pom` to regenerate the pom.xml
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [com.nearinfinity.honeycomb/honeycomb "0.1-SNAPSHOT"]
-                 [com.google.guava/guava "14.0.1"]
-                 [com.google.inject/guice "3.0"]
-                 [com.google.inject.extensions/guice-multibindings "3.0"]]
+                 [com.nearinfinity.honeycomb/honeycomb "0.1-SNAPSHOT" :scope "provided"]
+                 [com.google.guava/guava "14.0.1" :scope "provided"]
+                 [com.google.inject/guice "3.0" :scope "provided"]
+                 [com.google.inject.extensions/guice-multibindings "3.0" :scope "provided"]]
   :global-vars {*warn-on-reflection* true}
-  :aot :all
-  :parent  [com.nearinfinity.honeycomb/honeycomb-parent "0.1-SNAPSHOT" :relative-path "../../pom.xml"])
+  :aot :all)
