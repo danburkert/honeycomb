7cfafe458a16ac795a01e06dfe90292f1b8aeb5e
Andy Keffalas <akeffala@nearinfinity.com>
2013-05-20 18:18:09 -0400

    Updated environment variable check to refer to new variable name

--- scripts/utilities/plugin-build-install.sh
+++ scripts/utilities/plugin-build-install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-: ${HONEYCOMB_HOME?"Need to set HONEYCOMB_HOME environmental variable to the top of the project."}
-: ${MYSQL_HOME?"Need to set MYSQL_HOME environmental variable to MySQL's installation directory."}
+: ${HONEYCOMB_SOURCE?"Need to set HONEYCOMB_HOME environmental variable to the top of the project."}
+: ${MYSQL_SOURCE?"Need to set MYSQL_HOME environmental variable to MySQL's installation directory."}
 command -v cmake >/dev/null 2>&1 || { echo >&2 "cmake is required to run $0."; exit 1; }
 command -v make >/dev/null 2>&1 || { echo >&2 "make is required to run $0."; exit 1; }
-source $HONEYCOMB_HOME/scripts/utilities/constants.sh
+source $HONEYCOMB_SOURCE/scripts/utilities/constants.sh
 
 if [ $# -eq 1 ]
 then
