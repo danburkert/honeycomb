d0e252f019c2c1ba885eb89dee077837da110986
Andy Keffalas <akeffala@nearinfinity.com>
2013-05-28 14:06:05 -0400

    Added path to MySQL server startup script for execution to avoid being on environment's PATH

--- scripts/utilities/mysql-restart.sh
+++ scripts/utilities/mysql-restart.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-command -v mysql.server >/dev/null 2>&1 || { echo >&2 "mysql.server is required to run $0."; exit 1; }
+: ${MYSQL_HOME?"Need to set MYSQL_HOME environment variable to the MySQL installation location."}
+
 dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # hbase_processes=`$dir/hbase-processes.sh | wc -l`
 # [ $hbase_processes -eq 3 ] || { echo >&2 "HBase is not running or not running correctly. Only $hbase_processes processes found."; exit 1; }
 
-mysql.server stop
-mysql.server start --character-set-server=utf8 --collation-server=utf8_bin --plugin-load=Honeycomb=ha_honeycomb.so --default-storage-engine=Honeycomb
+MYSQL_STARTUP_SCRIPT=$MYSQL_HOME/support-files/mysql.server
+
+if [ ! -x $MYSQL_STARTUP_SCRIPT ]
+then
+    echo >&2 "$MYSQL_STARTUP_SCRIPT must be available and executable to run $0."
+    exit 1
+fi
+
+$MYSQL_STARTUP_SCRIPT stop
+$MYSQL_STARTUP_SCRIPT start --character-set-server=utf8 --collation-server=utf8_bin --plugin-load=Honeycomb=ha_honeycomb.so --default-storage-engine=Honeycomb
