fc2946732537173cbb96d05b03a21e9ff23fd828
Andy Keffalas <akeffala@nearinfinity.com>
2013-05-28 16:13:03 -0400

    Removed unnecessary server stop command since it likely won't respond if the issued kill fails

--- scripts/utilities/kill-mysqld.sh
+++ scripts/utilities/kill-mysqld.sh
@@ -2,9 +2,3 @@ ps ax | grep -E "/bin/(mysqld|mysqld_safe)" | awk '{ print $1 }' | while read pi
 if [ -e /tmp/mysql.sock ]; then
   sudo rm /tmp/mysql.sock
 fi
-
-process=`ps ax | grep -E "/bin/(mysqld|mysqld_safe)"`
-
-if [ ! -z $process ]; then
-  sudo mysql.server stop
-fi
