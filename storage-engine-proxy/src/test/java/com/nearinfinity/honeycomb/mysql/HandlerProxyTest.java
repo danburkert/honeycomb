ad6cc3d788be829d773dcd1e076a89e625ceef4d
Andy Keffalas <akeffala@nearinfinity.com>
2013-05-31 16:35:41 -0400

    Added test coverage for table open and index add/remove operations

--- storage-engine-proxy/src/test/java/com/nearinfinity/honeycomb/mysql/HandlerProxyTest.java
+++ storage-engine-proxy/src/test/java/com/nearinfinity/honeycomb/mysql/HandlerProxyTest.java
@@ -24,6 +24,7 @@ package com.nearinfinity.honeycomb.mysql;
 
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -51,10 +52,17 @@ import com.nearinfinity.honeycomb.mysql.schema.TableSchema;
 @RunWith(PowerMockRunner.class)
 public class HandlerProxyTest {
 
-    private static final String DUMMY_TABLE_NAME = "foo/bar";
+    private static final String TEST_INDEX = "testIdx";
+
+    private static final String TEST_COLUMN = "testCol";
+
+    private static final String TEST_TABLE_NAME = "foo/bar";
 
     private static final List<ColumnSchema> COLUMNS = ImmutableList.of(
-            ColumnSchema.builder("testCol", ColumnType.LONG).setIsAutoIncrement(true).build());
+            ColumnSchema.builder(TEST_COLUMN, ColumnType.LONG).setIsAutoIncrement(true).build());
+
+    private static final IndexSchema INDEX_SCHEMA = new IndexSchema(TEST_INDEX, ImmutableList.<String>of(TEST_COLUMN), false);
+    private static final TableSchema TABLE_SCHEMA = new TableSchema(COLUMNS, ImmutableList.<IndexSchema>of(INDEX_SCHEMA));
 
     @Mock
     private Store storageMock;
@@ -76,13 +84,13 @@ public class HandlerProxyTest {
 
     @Test
     public void testRenameTable() throws Exception {
-        when(storeFactory.createStore(DUMMY_TABLE_NAME)).thenReturn(storageMock);
+        when(storeFactory.createStore(TEST_TABLE_NAME)).thenReturn(storageMock);
 
         final String renamedTableName = "bar/baz";
 
-        proxy.renameTable(DUMMY_TABLE_NAME, renamedTableName);
+        proxy.renameTable(TEST_TABLE_NAME, renamedTableName);
 
-        verify(storageMock, times(1)).renameTable(eq(DUMMY_TABLE_NAME), eq(renamedTableName));
+        verify(storageMock, times(1)).renameTable(eq(TEST_TABLE_NAME), eq(renamedTableName));
     }
 
     @Test(expected = NullPointerException.class)
@@ -112,31 +120,29 @@ public class HandlerProxyTest {
 
     @Test(expected = NullPointerException.class)
     public void testCreateTableInvalidTableName() {
-        final TableSchema schema = new TableSchema(COLUMNS, ImmutableList.<IndexSchema>of());
-        proxy.createTable(null, schema.serialize(), 0);
+
+        proxy.createTable(null, TABLE_SCHEMA.serialize(), 0);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void testCreateTableEmptyTableName() {
-        final TableSchema schema = new TableSchema(COLUMNS, ImmutableList.<IndexSchema>of());
-        proxy.createTable("", schema.serialize(), 0);
+        proxy.createTable("", TABLE_SCHEMA.serialize(), 0);
     }
 
     @Test(expected = NullPointerException.class)
     public void testCreateTableInvalidTableSchema() {
-        proxy.createTable(DUMMY_TABLE_NAME, null, 0);
+        proxy.createTable(TEST_TABLE_NAME, null, 0);
     }
 
     @Test
     public void testCreateTable() {
         when(storeFactory.createStore(anyString())).thenReturn(storageMock);
-        final TableSchema schema = new TableSchema(COLUMNS, ImmutableList.<IndexSchema>of());
         final long incrementValue = 42;
-        proxy.createTable(DUMMY_TABLE_NAME, schema.serialize(), incrementValue);
+        proxy.createTable(TEST_TABLE_NAME, TABLE_SCHEMA.serialize(), incrementValue);
 
-        verify(storeFactory, times(1)).createStore(eq(DUMMY_TABLE_NAME));
-        verify(storageMock, times(1)).createTable(eq(DUMMY_TABLE_NAME), eq(schema));
-        verify(storageMock, times(1)).incrementAutoInc(eq(DUMMY_TABLE_NAME), eq(incrementValue));
+        verify(storeFactory, times(1)).createStore(eq(TEST_TABLE_NAME));
+        verify(storageMock, times(1)).createTable(eq(TEST_TABLE_NAME), eq(TABLE_SCHEMA));
+        verify(storageMock, times(1)).incrementAutoInc(eq(TEST_TABLE_NAME), eq(incrementValue));
     }
 
     @Test(expected = NullPointerException.class)
@@ -154,11 +160,135 @@ public class HandlerProxyTest {
         when(storeFactory.createStore(anyString())).thenReturn(storageMock);
         when(storageMock.openTable(anyString())).thenReturn(tableMock);
 
-        proxy.dropTable(DUMMY_TABLE_NAME);
+        proxy.dropTable(TEST_TABLE_NAME);
 
-        verify(storeFactory, times(1)).createStore(eq(DUMMY_TABLE_NAME));
-        verify(storageMock, times(1)).openTable(eq(DUMMY_TABLE_NAME));
+        verify(storeFactory, times(1)).createStore(eq(TEST_TABLE_NAME));
+        verify(storageMock, times(1)).openTable(eq(TEST_TABLE_NAME));
         verify(tableMock, times(1)).deleteAllRows();
-        verify(storageMock, times(1)).deleteTable(eq(DUMMY_TABLE_NAME));
+        verify(storageMock, times(1)).deleteTable(eq(TEST_TABLE_NAME));
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testOpenTableInvalidTableName() {
+        proxy.openTable(null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testOpenTableEmptyTableName() {
+        proxy.openTable("");
+    }
+
+    @Test
+    public void testOpenTable() {
+        when(storeFactory.createStore(anyString())).thenReturn(storageMock);
+        when(storageMock.openTable(anyString())).thenReturn(tableMock);
+
+        proxy.openTable(TEST_TABLE_NAME);
+
+        verify(storeFactory, times(1)).createStore(eq(TEST_TABLE_NAME));
+        verify(storageMock, times(1)).openTable(eq(TEST_TABLE_NAME));
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testAddIndexInvalidIndexName() {
+        proxy.addIndex(null, INDEX_SCHEMA.serialize());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testAddIndexEmptyIndexName() {
+        proxy.addIndex("", INDEX_SCHEMA.serialize());
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testAddIndexInvalidIndexSchema() {
+        proxy.addIndex(TEST_INDEX, null);
+    }
+
+    @Test
+    public void testAddIndex() {
+        when(storeFactory.createStore(anyString())).thenReturn(storageMock);
+        when(storageMock.openTable(anyString())).thenReturn(tableMock);
+
+        proxy.openTable(TEST_TABLE_NAME);
+
+        verify(storeFactory, times(1)).createStore(eq(TEST_TABLE_NAME));
+        verify(storageMock, times(1)).openTable(eq(TEST_TABLE_NAME));
+
+        proxy.addIndex(TEST_INDEX, INDEX_SCHEMA.serialize());
+
+        verify(storageMock, times(1)).addIndex(eq(TEST_TABLE_NAME), eq(INDEX_SCHEMA));
+        verify(tableMock, times(1)).insertTableIndex(eq(INDEX_SCHEMA));
+        verify(tableMock, times(1)).flush();
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testAddUniqueIndex() {
+        when(storeFactory.createStore(anyString())).thenReturn(storageMock);
+        when(storageMock.openTable(anyString())).thenReturn(tableMock);
+
+        proxy.openTable(TEST_TABLE_NAME);
+
+        verify(storeFactory, times(1)).createStore(eq(TEST_TABLE_NAME));
+        verify(storageMock, times(1)).openTable(eq(TEST_TABLE_NAME));
+
+        final IndexSchema uniqueIndex = new IndexSchema("uniqueIdx", ImmutableList.<String>of(TEST_COLUMN), true);
+        proxy.addIndex(TEST_INDEX, uniqueIndex.serialize());
+
+        verify(storageMock, never()).addIndex(eq(TEST_TABLE_NAME), eq(INDEX_SCHEMA));
+        verify(tableMock, never()).insertTableIndex(eq(INDEX_SCHEMA));
+        verify(tableMock, never()).flush();
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testAddIndexTableNotOpen() {
+        when(storeFactory.createStore(anyString())).thenReturn(storageMock);
+        when(storageMock.openTable(anyString())).thenReturn(tableMock);
+
+        proxy.addIndex(TEST_INDEX, INDEX_SCHEMA.serialize());
+
+        verify(storageMock, never()).addIndex(eq(TEST_TABLE_NAME), eq(INDEX_SCHEMA));
+        verify(tableMock, never()).insertTableIndex(eq(INDEX_SCHEMA));
+        verify(tableMock, never()).flush();
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testDropIndexInvalidIndexName() {
+        proxy.dropIndex(null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testDropIndexEmptyIndexName() {
+        proxy.dropIndex("");
+    }
+
+    @Test
+    public void testDropIndex() {
+        when(storeFactory.createStore(anyString())).thenReturn(storageMock);
+        when(storageMock.openTable(anyString())).thenReturn(tableMock);
+
+        proxy.openTable(TEST_TABLE_NAME);
+
+        verify(storeFactory, times(1)).createStore(eq(TEST_TABLE_NAME));
+        verify(storageMock, times(1)).openTable(eq(TEST_TABLE_NAME));
+
+        when(storageMock.getSchema(TEST_TABLE_NAME)).thenReturn(TABLE_SCHEMA);
+
+        proxy.dropIndex(TEST_INDEX);
+
+        verify(storageMock, times(1)).getSchema(eq(TEST_TABLE_NAME));
+        verify(tableMock, times(1)).deleteTableIndex(eq(INDEX_SCHEMA));
+        verify(storageMock, times(1)).dropIndex(eq(TEST_TABLE_NAME), eq(TEST_INDEX));
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testDropIndexTableNotOpen() {
+        when(storeFactory.createStore(anyString())).thenReturn(storageMock);
+        when(storageMock.openTable(anyString())).thenReturn(tableMock);
+
+        proxy.dropIndex(TEST_INDEX);
+
+        verify(storageMock, never()).getSchema(eq(TEST_TABLE_NAME));
+        verify(tableMock, never()).deleteTableIndex(eq(INDEX_SCHEMA));
+        verify(storageMock, never()).dropIndex(eq(TEST_TABLE_NAME), eq(TEST_INDEX));
     }
 }
