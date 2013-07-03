5587b96559bc0270d41f4fa0a820c4422a879035
Sean Howell <showell@nearinfinity.com>
2013-07-03 16:15:16 -0400

    Updated memory honeycomb version.

--- storage-engine-backends/memory/project.clj
+++ storage-engine-backends/memory/project.clj
@@ -14,17 +14,17 @@
  ; KIND, either express or implied.  See the License for the
  ; specific language governing permissions and limitations
  ; under the License.
- ; 
+ ;
  ; Copyright 2013 Near Infinity Corporation.
 
 
-(defproject com.nearinfinity.honeycomb.memory/honeycomb-memory "0.1"
+(defproject com.nearinfinity.honeycomb.memory/honeycomb-memory "0.2-SNAPSHOT"
   :description "In-memory backend for the Honeycomb storage engine"
   :url "http://www.github.com/nearinfinity/honeycomb"
   :license {:name "Apache License, Version 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [com.nearinfinity.honeycomb/honeycomb "0.1" :scope "provided"]
+                 [com.nearinfinity.honeycomb/honeycomb "0.2-SNAPSHOT" :scope "provided"]
                  [com.google.guava/guava "14.0.1" :scope "provided"]
                  [com.google.inject/guice "3.0" :scope "provided"]
                  [com.google.inject.extensions/guice-multibindings "3.0" :scope "provided"]]
